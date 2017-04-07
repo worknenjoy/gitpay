2db107271f19473d82cb942797ca468578e062e9
wharley <wharleyangelica@gmail.com>
2017-04-06 21:11:16 -0300

    #1 Angular install with sucess/Angular testing with server

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+'use strict'
+
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//Angular testing with server
+app.use(express.static(__dirname + '/myGitPay/dist/'));
+
+//Jade - comments to test the Angular
+//======================================================
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+//app.get('/', function(request, response) {
+//  response.render('pages/index');
+//});
+//======================================================
+
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
---
+++ myGitPay/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { PageComponent } from './page/page.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
