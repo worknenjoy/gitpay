87127845b05d014f61eb159e8fc3e1034874d6a4
wharley <wharleyangelica@gmail.com>
2017-04-11 02:25:53 -0300

    #3 Loading the configuration of orm sequelize

---
+++ loading/loading.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const env = process.env.NODE_ENV || "development";
+const config = require('../config/config.json')[env];
+const path = require("path");
+let Sequelize = require("sequelize");
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+let db = {};
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
