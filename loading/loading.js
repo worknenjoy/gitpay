21f74df3c562e58ca6dd60dc9c6eac4a102238d9
wharley <wharleyangelica@gmail.com>
2017-04-11 02:30:38 -0300

    #3 Pattern JavaScript

--- loading/loading.js
+++ loading/loading.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
-const path = require("path");
-let Sequelize = require("sequelize");
+const path = require('path');
+let Sequelize = require('sequelize');
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 let db = {};
 
