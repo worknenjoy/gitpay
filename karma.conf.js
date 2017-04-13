2fb51cc451962dc27c652b0a03ec7cc20243f2d5
Alexandre Magno <alexanmtz@gmail.com>
2017-04-13 16:19:02 +0200

    a more complete travis configuration to run karma on the ci

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
     singleRun: true,
     customLaunchers: {
