bc58dab52d87750fc08c60831b3bedcaf3b84477
Alexandre Magno <alexanmtz@gmail.com>
2017-11-18 02:37:41 +0100

    fixing tests

---
+++ config/secrets.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+
+const databaseDev = {
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gitPay',
+  host: '127.0.0.1',
+  port: 5432,
+  dialect: 'postgres',
+  logging: false
+}
+
+const databaseTest = {
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gitPay',
+  host: '127.0.0.1',
+  port: 5432,
+  dialect: 'postgres',
+  logging: false
+}
+
+const databaseProd = {
+  username: 'root',
+  password: null,
+  database: 'gitPay',
+  schema: 'public',
+  host: '127.0.0.1',
+  port: 5432,
+  dialect: 'postgres',
+  logging: false
+}
+
+const facebook = {
+  id: process.env.FACEBOOK_ID,
+  secret: process.env.FACEBOOK_SECRET
+}
+
+const google = {
+  id: process.env.GOOGLE_ID,
+  secret: process.env.GOOGLE_SECRET
+}
+
+const github = {
+  id: process.env.GITHUB_ID,
+  secret: process.env.GITHUB_SECRET
+}
+
+const oauthCallbacks = {
+  googleCallbackUrl: 'http://localhost:5000/callback/google',
+  githubCallbackUrl: 'http://localhost:5000/callback/github',
+  facebookCallbackUrl: 'http://localhost:5000/callback/facebook'
+}
+
+module.exports = { databaseDev, databaseTest, databaseProd, facebook, google, github, oauthCallbacks }
