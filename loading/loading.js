b7959b2e6a7889b456e858d9fd363a98d13fa0cc
Alexandre Magno <alexanmtz@gmail.com>
2017-11-20 20:54:33 +0100

    loading database in production

--- loading/loading.js
+++ loading/loading.js
@@ -3,11 +3,35 @@
 const fs = require('fs');
 const path = require('path');
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'databaseDev';
-const config = require('../config/secrets')[env];
+const env = process.env.NODE_ENV || 'development';
+const database_env = {
+  'development': 'databaseDev',
+  'production': 'databaseProd',
+  'test': 'databaseTest'
+};
+const config = require('./config/secrets')[database_env[env]];
 
 let Sequelize = require('sequelize');
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize = {};
+
+if (env == 'production') {
+  const database_url = process.env.DATABASE_URL;
+  const database_settings = database_url.split(':');
+  const port = database_settings[4];
+  const host = database_settings[3];
+  sequelize = new Sequelize(database_url, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     port,
+    host:     host,
+    logging:  true //false
+  });
+  console.log('running production migration');
+
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 let db = {};
 
 fs
