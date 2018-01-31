ce9f95319ec6a730e3a0ecf1a92a7b6ce4ed2165
Alexandre Magno <alexanmtz@gmail.com>
2018-01-31 17:45:06 +0100

    more blocks of content explaining why the product is so great

--- frontend/src/components/welcome/welcome.js
+++ frontend/src/components/welcome/welcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from 'material-ui/styles'
 import Grid from 'material-ui/Grid'
 import Typography from 'material-ui/Typography'
+import Divider from 'material-ui/Divider';
 import Button from 'material-ui/Button'
 import List, { ListItem, ListItemText, ListItemIcon } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
@@ -19,6 +20,8 @@ const logoGitlab = require('../../images/gitlab-logo.png')
 const logoGithub = require('../../images/github-logo.png')
 const logoBitbucket = require('../../images/bitbucket-logo.png')
 const octodex = require('../../images/octodex.png')
+const octodexMotherhubbertocat = require('../../images/octodex-motherhubbertocat.png')
+const deal = require('../../images/deal.png')
 
 const styles = theme => ({
   root: {
@@ -55,18 +58,23 @@ const styles = theme => ({
     backgroundColor: '#f1f0ea'
   },
   divider: {
-    marginTop: 120,
     textAlign: 'center',
     display: 'inline-block',
     marginLeft: '35%',
     marginBottom: '5%',
+    marginTop: 20,
+    fontSize: 18,
+    paddingBottom: 10,
+    borderBottom: '5px solid black',
     width: '25%',
-    height: 5,
-    backgroundColor: theme.palette.text.primary
   },
   mainlist: {
     textAlign: 'left',
     marginLeft: '20%',
+    width: '100%'
+  },
+  seclist: {
+    textAlign: 'left',
     width: '100%'
   },
   listIconTop: {
@@ -169,7 +177,11 @@ function Welcome(props) {
         <div className={classes.secBlock}>
           <Grid container spacing={24}>
               <Grid item xs={12} sm={6}>
-                <div className={classes.divider}></div>
+                <div className={classes.divider}>
+                  <Typography type="headline" gutterBottom>
+                    Para devs
+                  </Typography>
+                </div>
                 <div className={classes.mainlist}>
                   <List>
                     <ListItem className={classes.listIconTop}>
@@ -202,6 +214,90 @@ function Welcome(props) {
               <Grid item xs={12} sm={6}>
                 <img width="600" src={octodex} />
               </Grid>
+          </Grid>
+        </div>
+        <div className={classes.mainBlock}>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={6}>
+              <div className={classes.divider}>
+                <Typography type="headline" gutterBottom>
+                  Para empresas
+                </Typography>
+              </div>
+              <div className={classes.mainlist}>
+                <List>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <AppsIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Gerencie o desenvolvimento da sua startup" secondary="Com o Gitpay as empresas podem gerir seu negócio utilizando ferramentas de desenvolvimento feito sobe medida para você" />
+                  </ListItem>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <WorkIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Pague pelos trabalhos concluídos ou estimule desenvolvedores a contribuir com seu projeto usando contratos inteligentes" secondary="Envolva diferentes tipos de desenvolvendores, com experiências variadas que irão ajudar no desenvolvimento do projeto usando as ferramentas que ele conhece e processos já consolidados" />
+                  </ListItem>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <AccountBalanceWalletIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Desenvolva o seu negócio com ferramentas open source, e pagando por servicos sob demanda" secondary="Empresas podem nascer e se manter com o Gitpay, pagando apenas pelas demandas concluídas e integradas com o projeto" />
+                  </ListItem>
+                </List>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <img width="500" src={octodexMotherhubbertocat} />
+            </Grid>
+          </Grid>
+        </div>
+        <div className={classes.mainBlock}>
+          <Grid container spacing={24}>
+            <div className={classes.divider}>
+              <Typography type="headline" gutterBottom>
+                Integracoes
+              </Typography>
+            </div>
+            <Grid item xs={12} sm={6}>
+              <img width="400" src={deal} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div className={classes.seclist}>
+                <List>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <AppsIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Gerencie o desenvolvimento da sua startup" secondary="Com o Gitpay as empresas podem gerir seu negócio utilizando ferramentas de desenvolvimento feito sobe medida para você" />
+                  </ListItem>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <WorkIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Pague pelos trabalhos concluídos ou estimule desenvolvedores a contribuir com seu projeto usando contratos inteligentes" secondary="Envolva diferentes tipos de desenvolvendores, com experiências variadas que irão ajudar no desenvolvimento do projeto usando as ferramentas que ele conhece e processos já consolidados" />
+                  </ListItem>
+                  <ListItem className={classes.listIconTop}>
+                    <ListItemIcon>
+                      <Avatar>
+                        <AccountBalanceWalletIcon />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText primary="Desenvolva o seu negócio com ferramentas open source, e pagando por servicos sob demanda" secondary="Empresas podem nascer e se manter com o Gitpay, pagando apenas pelas demandas concluídas e integradas com o projeto" />
+                  </ListItem>
+                </List>
+              </div>
+            </Grid>
           </Grid>
         </div>
       </div>
