3771759cef10de4b36c29d94dee13c8336fe5506
Alexandre Magno <alexanmtz@gmail.com>
2018-05-29 14:21:03 +0200

    generating a production frontend with the app store enhancer with devtools

--- frontend/src/main/app.js
+++ frontend/src/main/app.js
@@ -19,14 +19,27 @@ if(process.ENV == 'production') {
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunkMiddleware),
+  // other store enhancers if any
+);
+
 const store = createStore(
   reducers,
-  compose(
+  enhancer
+  /*compose(
     applyMiddleware(
       thunkMiddleware
     ),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  )*/
 );
 
 const theme = createMuiTheme(Palette);
