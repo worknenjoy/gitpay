1de5dd8deff2220544fff654990c8c2449425fc8
Alexandre Magno <alexanmtz@gmail.com>
2018-05-29 18:45:55 +0200

    loading stripe js inside the component

--- frontend/src/components/checkout/stripe-checkout.js
+++ frontend/src/components/checkout/stripe-checkout.js
@@ -10,14 +10,22 @@ class StripeCheckout extends Component {
   }
 
   componentWillMount() {
-    if (window.Stripe) {
-      this.setState({stripe: window.Stripe('pk_test_pBA57lmPZbGhidkUUphTZZdB')});
-    } else {
-      document.querySelector('#stripe-js').addEventListener('load', () => {
-        // Create Stripe instance once Stripe.js loads
-        this.setState({stripe: window.Stripe('pk_test_pBA57lmPZbGhidkUUphTZZdB')});
+    // componentDidMount only runs in a browser environment.
+    // In addition to loading asynchronously, this code is safe to server-side render.
+
+    // You can inject a script tag manually like this,
+    // or you can use the 'async' attribute on the Stripe.js v3 <script> tag.
+    const stripeJs = document.createElement('script');
+    stripeJs.src = 'https://js.stripe.com/v3/';
+    stripeJs.async = true;
+    stripeJs.onload = () => {
+      // The setTimeout lets us pretend that Stripe.js took a long time to load
+      // Take it out of your production code!
+      this.setState({
+        stripe: window.Stripe('pk_test_pBA57lmPZbGhidkUUphTZZdB'),
       });
     }
+    document.body && document.body.appendChild(stripeJs);
   }
 
   render() {
