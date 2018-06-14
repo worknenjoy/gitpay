0b8254f156bee6cb7bd89b6434b88cf4729bd2ad
wharleyornelasjs <wharleyornelasjs@gmail.com>
2018-06-14 00:58:50 -0300

    Updated configuration server.

--- loading/loading.js
+++ loading/loading.js
@@ -1,56 +1,57 @@
-'use strict';
+const fs = require('fs')
+const path = require('path')
+const basename = path.basename(module.filename)
+const env = process.env.NODE_ENV || 'development'
 
-const fs = require('fs');
-const path = require('path');
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
 const database_env = {
   'development': 'databaseDev',
   'production': 'databaseProd',
   'test': 'databaseTest'
-};
-const config = require('../config/secrets')[database_env[env]];
+}
 
-let Sequelize = require('sequelize');
-let sequelize = {};
+const config = require('../config/secrets')[database_env[env]]
+
+let Sequelize = require('sequelize')
+let sequelize = {}
 
 if (env == 'production') {
-  const database_url = process.env.DATABASE_URL;
-  const database_settings = database_url.split(':');
-  const port = database_settings[4];
-  const host = database_settings[3];
+  const database_url = process.env.DATABASE_URL
+  const database_settings = database_url.split(':')
+  const port = database_settings[4]
+  const host = database_settings[3]
   sequelize = new Sequelize(database_url, {
-    dialect:  'postgres',
+    dialect: 'postgres',
     protocol: 'postgres',
-    port:     port,
-    host:     host,
-    logging:  true //false
-  });
+    port: port,
+    host: host,
+    logging: true // false
+  })
+  // eslint-disable-next-line no-console
   console.log('running production migration');
 
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 
-let db = {};
+let db = {}
 
 fs
-    .readdirSync(__dirname)
-    .filter((file) => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach((file) => {
-        const model = sequelize['import'](path.join(__dirname, file));
-        db[model.name] = model;
-    });
+  .readdirSync(__dirname)
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file))
+    db[model.name] = model
+  })
 
-Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-});
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-'use strict'
-
-const express = require('express');
-const cors = require('cors');
-const sslRedirect = require('heroku-ssl-redirect');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const sslRedirect = require('heroku-ssl-redirect')
+const app = express()
 const session = require('express-session')
-const bodyParser = require('body-parser');
-const loading = require('./loading/loading');
-const passport = require('passport');
-const passportConfig = require('./config/passport');
-const load = require('./modules/load/load');
-const feed = require('feed-read');
+const bodyParser = require('body-parser')
+require('./loading/loading')
+const passport = require('passport')
+require('./config/passport')
+const load = require('./modules/load/load')
+const feed = require('feed-read')
 
-if(process.env.NODE_ENV != 'production') {
-  app.use(cors());
+if (process.env.NODE_ENV != 'production') {
+  app.use(cors())
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
   secret: process.env.SECRET_PHRASE
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(sslRedirect())
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3000))
 
-app.use(express.static(__dirname + '/frontend/public/'));
+app.use(express.static(`${__dirname}/frontend/public/`))
 
-app.get('/octos', function (req, res) {
+app.get('/octos', (req, res) => {
+  feed('http://feeds.feedburner.com/Octocats', (err, articles) => {
+    if (err) throw err
 
-    feed("http://feeds.feedburner.com/Octocats", (err, articles) => {
-        if (err) throw err;
+    // eslint-disable-next-line no-console
+    console.log(articles)
+  })
 
-        console.log(articles);
+  return res.json({}).end()
+})
 
-    });
-
-    return res.json({
-
-    }).end();
-});
-
-load.init(app);
+load.init(app)
 
 app.listen(app.get('port'), () => {
-    console.log('Node app is running on port', app.get('port'));
-});
+  // eslint-disable-next-line no-console
+  console.log('Node app is running on port', app.get('port'))
+})
 
 module.exports = app
