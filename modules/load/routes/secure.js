022613c59c73ab2e433627fed97ba73461cd2d1e
wharleyornelasjs <wharleyornelasjs@gmail.com>
2018-06-14 21:56:46 -0300

    Added secure to protect to route.

---
+++ modules/load/routes/secure.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  // CORS preflight request
+  if (req.method === 'OPTIONS') {
+    next()
+  }
+  else {
+    const token = req.body.token || req.query.token || req.headers['authorization']
+
+    if (!token) return res.status(403).send({ errors: ['No token provided'] })
+
+    jwt.verify(token, process.env.SECRET_PHRASE, (err, decoded) => {
+      if (err) return res.status(403).send({ errors: ['Failed to authenticate token'] })
+
+      req.decoded = decoded
+      next()
+    })
+  }
+}
