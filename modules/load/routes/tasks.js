3c8560bd668eb42869206bca0cf27ab3736cff97
Alexandre Magno <alexanmtz@gmail.com>
2018-06-16 15:45:26 +0200

    secure just the required task routes and not the public ones

--- modules/load/routes/tasks.js
+++ modules/load/routes/tasks.js
@@ -4,13 +4,13 @@ const authenticationHelpers = require('../../authenticationHelpers')
 const controllers = require('../controllers/task')
 const secure = require('./secure')
 
-router.use(secure)
-
-router.post('/create', controllers.createTask)
-router.get('/list', controllers.listTasks)
 router.get('/fetch/:id', controllers.fetchTask)
+router.get('/:id/sync/:field', controllers.syncTask)
+router.get('/list', controllers.listTasks)
+
+router.use(secure)
+router.post('/create', controllers.createTask)
 router.put('/update', controllers.updateTask)
 router.post('/payments', controllers.paymentTask)
-router.get('/:id/sync/:field', controllers.syncTask)
 
 module.exports = router
