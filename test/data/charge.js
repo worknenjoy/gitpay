141702200dcaa84ad949a176eea25cbf4b8a2d7c
Alexandre Magno <alexanmtz@gmail.com>
2018-06-18 20:34:46 +0200

    adding more webhooks

--- test/data/charge.js
+++ test/data/charge.js
@@ -93,3 +93,97 @@ module.exports.update = {
   "type": "charge.updated"
 }
 
+module.exports.success = {
+  "id": "evt_1CeLZlBrSjgsps2DYpOlFCuW",
+  "object": "event",
+  "api_version": "2018-02-28",
+  "created": 1529321473,
+  "data": {
+    "object": {
+      "id": "ch_1CeLZkBrSjgsps2DCNBQmnLA",
+      "object": "charge",
+      "amount": 500,
+      "amount_refunded": 0,
+      "application": null,
+      "application_fee": null,
+      "balance_transaction": "txn_1CeLZlBrSjgsps2DYNYEZqhs",
+      "captured": true,
+      "created": 1529321472,
+      "currency": "usd",
+      "customer": "cus_CyvwcNX1GNZYpi",
+      "description": null,
+      "destination": null,
+      "dispute": null,
+      "failure_code": null,
+      "failure_message": null,
+      "fraud_details": {
+      },
+      "invoice": null,
+      "livemode": true,
+      "metadata": {
+        "order_id": "7"
+      },
+      "on_behalf_of": null,
+      "order": null,
+      "outcome": {
+        "network_status": "approved_by_network",
+        "reason": null,
+        "risk_level": "normal",
+        "seller_message": "Payment complete.",
+        "type": "authorized"
+      },
+      "paid": true,
+      "receipt_email": null,
+      "receipt_number": null,
+      "refunded": false,
+      "refunds": {
+        "object": "list",
+        "data": [
+        ],
+        "has_more": false,
+        "total_count": 0,
+        "url": "/v1/charges/ch_1CeLZkBrSjgsps2DCNBQmnLA/refunds"
+      },
+      "review": null,
+      "shipping": null,
+      "source": {
+        "id": "card_1CeLZgBrSjgsps2D46GUqEBB",
+        "object": "card",
+        "address_city": null,
+        "address_country": null,
+        "address_line1": null,
+        "address_line1_check": null,
+        "address_line2": null,
+        "address_state": null,
+        "address_zip": null,
+        "address_zip_check": null,
+        "brand": "Visa",
+        "country": "DK",
+        "customer": "cus_CyvwcNX1GNZYpi",
+        "cvc_check": "pass",
+        "dynamic_last4": null,
+        "exp_month": 10,
+        "exp_year": 2019,
+        "fingerprint": "qwJTFcMNAfCltmXY",
+        "funding": "debit",
+        "last4": "7743",
+        "metadata": {
+        },
+        "name": "Alexandre Magno",
+        "tokenization_method": null
+      },
+      "source_transfer": null,
+      "statement_descriptor": null,
+      "status": "succeeded",
+      "transfer_group": "task_25"
+    }
+  },
+  "livemode": true,
+  "pending_webhooks": 1,
+  "request": {
+    "id": "req_a3oEDW4y0pfI9z",
+    "idempotency_key": null
+  },
+  "type": "charge.succeeded"
+}
+
--- test/webhook.test.js
+++ test/webhook.test.js
@@ -78,6 +78,38 @@ describe("webhooks", () => {
       });
     })
 
+    it('should update the order when a webhook charge.succeeded is triggered', (done) => {
+      models.User.build({email: 'teste@mail.com', password: 'teste'}).save().then((user) => {
+        models.Task.build({url: 'https://github.com/worknenjoy/truppie/issues/99', provider: 'github', userId: user.dataValues.id}).save()
+          .then((task) => {
+            task.createOrder({
+              source_id: 'card_1CeLZgBrSjgsps2D46GUqEBB',
+              currency: 'BRL',
+              amount: 200,
+              source: 'ch_1CeLZkBrSjgsps2DCNBQmnLA',
+              userId: user.dataValues.id
+            }).then((order) => {
+              agent
+                .post('/webhooks')
+                .send(chargeData.success)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                  expect(res.statusCode).to.equal(200);
+                  expect(res.body).to.exist;
+                  expect(res.body.id).to.equal('evt_1CeLZlBrSjgsps2DYpOlFCuW');
+                  models.Order.findById(order.dataValues.id).then((o) => {
+                    expect(o.dataValues.source).to.equal('ch_1CeLZkBrSjgsps2DCNBQmnLA');
+                    expect(o.dataValues.paid).to.equal(true);
+                    expect(o.dataValues.status).to.equal('succeeded');
+                    done();
+                  });
+                })
+            });
+          });
+      });
+    })
+
     describe('webhooks for transfer', () => {
       it('should notify the transfer when a webhook transfer.update is triggered', (done) => {
         models.User.build({email: 'teste@mail.com', password: 'teste'}).save().then((user) => {
@@ -114,6 +146,23 @@ describe("webhooks", () => {
               expect(res.statusCode).to.equal(200);
               expect(res.body).to.exist;
               expect(res.body.id).to.equal('evt_1CdprOLlCJ9CeQRe4QDlbGRY');
+              done();
+            })
+        })
+
+      })
+
+      it('should notify the transfer when a webhook payout.done is triggered', (done) => {
+        models.User.build({email: 'teste@mail.com', password: 'teste', account_id: 'acct_1CZ5vkLlCJ9CeQRe'}).save().then((user) => {
+          agent
+            .post('/webhooks')
+            .send(payoutData.done)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body).to.exist;
+              expect(res.body.id).to.equal('evt_1CeM4PLlCJ9CeQReQrtxB9GJ');
               done();
             })
         })
