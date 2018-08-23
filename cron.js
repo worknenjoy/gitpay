b60a4f950133efb61c8aa59f043817d317695494
Alexandre Magno <alexanmtz@gmail.com>
2018-08-23 20:43:20 +0200

    Forgot that is not semicolon here :p

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ const job = new CronJob({
   // Seconds: 0-59   Minutes: 0-59   Hours: 0-23   Day of Month: 1-31   Months: 0-11 (Jan-Dec)   Day of Week: 0-6 (Sun-Sat)
   cronTime: '0 0 0 * * *', // everyday at 12:00AM
   onTick: () => {
-    const d = new Date();
+    const d = new Date()
     // eslint-disable-next-line no-console
     console.log('Log to confirm cron job run at', d)
   }
