763aedddc55703134bd3318c1f032c81fa7e4de1
Alexandre Magno <alexanmtz@gmail.com>
2018-08-23 20:41:18 +0200

    Activate a cron to Run everyday at 12:00AM and logging

--- cron.js
+++ cron.js
@@ -2,10 +2,11 @@ const CronJob = require('cron').CronJob
 
 const job = new CronJob({
   // Seconds: 0-59   Minutes: 0-59   Hours: 0-23   Day of Month: 1-31   Months: 0-11 (Jan-Dec)   Day of Week: 0-6 (Sun-Sat)
-  cronTime: '4 * * * * * ',
+  cronTime: '0 0 0 * * *', // everyday at 12:00AM
   onTick: () => {
+    const d = new Date();
     // eslint-disable-next-line no-console
-    console.log('Execute your function')
+    console.log('Log to confirm cron job run at', d)
   }
 })
 
