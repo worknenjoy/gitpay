d18e8f2e8764cb6ad6f76307ad711811c9b82428
Alexandre Magno <alexanmtz@gmail.com>
2018-09-13 21:20:10 +0200

    updating gh-pages settings to sync with master

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -12,21 +12,30 @@ pwd
 remote=$(git config remote.origin.url)
 
 # make a directory to put the gp-pages branch
-#mkdir gh-pages-branch
-#cd gh-pages-branch
+mkdir gh-pages-branch
+cd gh-pages-branch
 # now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "alexanmtz@gmail.com" > /dev/null 2>&1
-git config --global user.name "Alexandre Magno" > /dev/null 2>&1
-#git init
-#git remote add --fetch origin "$remote"
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git init
+git remote add --fetch origin "$remote"
 
 
 # switch into the the gh-pages branch
-# git checkout gh-pages
+if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+then
+    git checkout gh-pages
+    # delete any old site as we are going to replace it
+    # Note: this explodes if there aren't any, so moving it here for now
+    # git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
 
 # copy over or recompile the new site
 #cp -a "../${siteSource}/." .
-#bundle exec jekyll build
+git pull origin gh-pages
+bundle exec jekyll build
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
@@ -36,7 +45,7 @@ git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 git push origin gh-pages
-#cd ..
-#rm -rf gh-pages-branch
+cd ..
+rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
