0c91d4ea6d32ce5d03187c6b6f76fe298a0bc36e
Alexandre Magno <alexanmtz@gmail.com>
2019-04-26 17:49:50 +0200

    fixing lint issues

--- frontend/sitemap-builder.js
+++ frontend/sitemap-builder.js
@@ -1,21 +1,19 @@
-require("babel-register")({
-    "ignore": [
-		"node_modules/**/*"
-	],
-    presets: ["es2015", "react"],
-    plugins: [
-        "transform-object-rest-spread",
-        "transform-class-properties"
-      ]
-});
+require('babel-register')({
+  'ignore': [
+    'node_modules/**/*'
+  ],
+  presets: ['es2015', 'react'],
+  plugins: [
+    'transform-object-rest-spread',
+    'transform-class-properties'
+  ]
+})
 
 const router = require('./src/main/routes-sitemap').default
-const Sitemap = require('react-router-sitemap').default;
+const Sitemap = require('react-router-sitemap').default
 
-console.log(router);
- 
 (
-    new Sitemap(router)
-        .build('http://gitpay.me')
-        .save('./sitemap.xml')
-);
+  new Sitemap(router)
+    .build('http://gitpay.me')
+    .save('./sitemap.xml')
+)
