10bc08d7168e027b57812d89fa229a9ea0e8b97b
Alexandre Magno <alexanmtz@gmail.com>
2019-05-20 19:44:30 +0200

    tests to actions and reducers to retrieve organizations

--- frontend/tests/actions/actions.test.js
+++ frontend/tests/actions/actions.test.js
@@ -2,7 +2,8 @@ jest.unmock('react-intl')
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import moxios from 'moxios'
-import * as actions from '../../src/actions/preferencesActions'
+import * as preferencesActions from '../../src/actions/preferencesActions'
+import * as organizationsActions from '../../src/actions/organizationsActions'
 import Auth from '../../src/modules/auth'
 
 Auth.getToken = () => true
@@ -10,32 +11,99 @@ Auth.getToken = () => true
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
-describe('actions', () => {
-  beforeEach(() => {
-
-  })
-  describe('country and language preference actions', () => {
-    it('should dispatch a action to get the current language', () => {
-      expect(actions.fetchPreferencesRequested()).toEqual({
-        type: 'FETCH_PREFERENCES_REQUESTED',
-        completed: false
+describe('action', () => {
+  describe('preference actions', () => {
+    beforeEach(() => {
+  
+    })
+    describe('country and language preference actions', () => {
+      it('should dispatch a action to get the current language', () => {
+        expect(preferencesActions.fetchPreferencesRequested()).toEqual({
+          type: 'FETCH_PREFERENCES_REQUESTED',
+          completed: false
+        })
+        expect(preferencesActions.fetchPreferencesSuccess({
+          language: 'br'
+        })).toEqual({
+          type: 'FETCH_PREFERENCES_SUCCESS',
+          completed: true,
+          language: 'br'
+        })
+        expect(preferencesActions.fetchPreferencesError({ error: true })).toEqual({
+          type: 'FETCH_PREFERENCES_ERROR',
+          completed: true,
+          error: { error: true }
+        })
       })
-      expect(actions.fetchPreferencesSuccess({
-        language: 'br'
-      })).toEqual({
-        type: 'FETCH_PREFERENCES_SUCCESS',
-        completed: true,
-        language: 'br'
-      })
-      expect(actions.fetchPreferencesError({ error: true })).toEqual({
-        type: 'FETCH_PREFERENCES_ERROR',
-        completed: true,
-        error: { error: true }
+    })
+    describe('async actions', () => {
+      describe('fetch language', () => {
+        beforeEach(() => {
+          moxios.install()
+        })
+  
+        afterEach(() => {
+          moxios.uninstall()
+        })
+  
+        it('fetches the language successfully', () => {
+          moxios.stubRequest('http://localhost:3000/authenticated', {
+            status: 200,
+            response: {
+              authenticated: true,
+              user: {
+                id: 1
+              }
+            }
+          })
+          moxios.wait(() => {
+            const request = moxios.requests.mostRecent()
+            request.respondWith({
+              status: 200,
+              response: {
+                language: 'br'
+              }
+            })
+          })
+          const expectedActions = [
+            { completed: false, logged: false, type: 'LOGGED_IN_REQUESTED' },
+            { open: true, text: 'user.login.successfull', type: 'ADD_NOTIFICATION' },
+            { completed: true, logged: true, type: 'LOGGED_IN_SUCCESS', user: { id: 1 } },
+            { completed: false, type: 'FETCH_PREFERENCES_REQUESTED' },
+            { completed: true, type: 'FETCH_PREFERENCES_SUCCESS', language: 'br' }
+          ]
+          const store = mockStore({ intl: { messages: {} }, preferences: { language: {} }, loggedIn: { logged: true, user: { id: 1 } } })
+          return store.dispatch(preferencesActions.fetchPreferences(1)).then(() => {
+            // return of async actions
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+        })
       })
     })
   })
-  describe('async actions', () => {
-    describe('fetch language', () => {
+  describe('organization actions', () => {
+    describe('should get organizations from that user', () => {
+      it('should dispatch a action to get the organizations', () => {
+        expect(organizationsActions.fetchOrganizationsRequested()).toEqual({
+          type: 'FETCH_ORGANIZATIONS_REQUESTED',
+          completed: false
+        })
+        expect(organizationsActions.fetchOrganizationsSuccess({
+          organizations: []
+        })).toEqual({
+          type: 'FETCH_ORGANIZATIONS_SUCCESS',
+          completed: true,
+          organizations: []
+        })
+        expect(organizationsActions.fetchOrganizationsError({ error: true })).toEqual({
+          type: 'FETCH_ORGANIZATIONS_ERROR',
+          completed: true,
+          error: { error: true }
+        })
+      })
+    })
+    describe('async actions', () => {
+      
       beforeEach(() => {
         moxios.install()
       })
@@ -43,14 +111,14 @@ describe('actions', () => {
       afterEach(() => {
         moxios.uninstall()
       })
-
-      it('fetches the language successfully', () => {
+      
+      it('fetches the organizations successfully', () => {
         moxios.stubRequest('http://localhost:3000/authenticated', {
           status: 200,
           response: {
             authenticated: true,
             user: {
-              id: 1
+              id: 2
             }
           }
         })
@@ -59,19 +127,21 @@ describe('actions', () => {
           request.respondWith({
             status: 200,
             response: {
-              language: 'br'
+              organizations: [
+                {name: 'test org'}
+              ]
             }
           })
         })
         const expectedActions = [
           { completed: false, logged: false, type: 'LOGGED_IN_REQUESTED' },
-          { open: true, text: 'user.login.successfull', type: 'ADD_NOTIFICATION' },
-          { completed: true, logged: true, type: 'LOGGED_IN_SUCCESS', user: { id: 1 } },
-          { completed: false, type: 'FETCH_PREFERENCES_REQUESTED' },
-          { completed: true, type: 'FETCH_PREFERENCES_SUCCESS', language: 'br' }
+          //{ open: true, text: 'user.login.successfull', type: 'ADD_NOTIFICATION' },
+          { completed: true, logged: true, type: 'LOGGED_IN_SUCCESS', user: { id: 2 } },
+          { completed: false, type: 'FETCH_ORGANIZATIONS_REQUESTED' },
+          { completed: true, type: 'FETCH_ORGANIZATIONS_SUCCESS', organizations: [{name: 'test org'}] }
         ]
-        const store = mockStore({ intl: { messages: {} }, preferences: { language: {} }, loggedIn: { logged: true, user: { id: 1 } } })
-        return store.dispatch(actions.fetchPreferences(1)).then(() => {
+        const store = mockStore({ intl: { messages: {} }, organizations: [{name: 'test org'}], loggedIn: { logged: true, user: { id: 2 } } })
+        return store.dispatch(organizationsActions.fetchOrganizations(2)).then(() => {
           // return of async actions
           expect(store.getActions()).toEqual(expectedActions)
         })
