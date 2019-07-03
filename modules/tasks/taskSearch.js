0f9904b6b967ef119588d7505de3c8c4cb47dce4
Alexandre Magno <alexanmtz@gmail.com>
2019-07-03 10:25:50 +0200

    default order when list tasks is desc

--- modules/tasks/taskSearch.js
+++ modules/tasks/taskSearch.js
@@ -4,7 +4,12 @@ const models = require('../../models')
 module.exports = Promise.method(function taskSearch () {
   return models.Task
     .findAll(
-      { include: [ models.User, models.Order, models.Assign ] }
+      { 
+        include: [ models.User, models.Order, models.Assign ],  
+        order: [
+            ['status', 'DESC'],
+        ]
+      }
     )
     .then(data => {
       return data
