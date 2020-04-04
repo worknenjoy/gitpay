3820ab3a14ec55eb9c08d846b78319dc92fa22c4
Alexandre Magno <alexanmtz@gmail.com>
2020-04-04 14:12:26 +0200

    new scripts file to run commands and fix internationalization issues when activate accounts

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const models = require('./models')
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+
+console.log('arguments', process.argv[2]);
+
+const scripts = {
+  accountInfo: () => {
+    models.User
+      .findAll(
+        {}
+      )
+      .then(users => {
+        if (!users) return false
+
+        if (users.length <= 0) return false
+
+        const accountInfo = users.map(u => {
+          if(!u.account_id) {
+            return { 
+              user: u.email,
+              active_account: false
+             }
+          }
+          const accountDetails = stripe.accounts.retrieve(u.account_id).then(account => {
+            console.log('account', account)
+            return {
+              user: u.email,
+              active_account: true,
+              account: account
+            }
+          })
+          return accountDetails
+        })
+        // eslint-disable-next-line no-console
+        console.log('users', accountInfo)
+        return accountInfo
+
+      }).catch(error => {
+        // eslint-disable-next-line no-console
+        console.log('error when search user: ', error)
+        return false
+      })
+  }
+}
+
+if(process.argv[2]) scripts[process.argv[2]]()
+
