29570a4050c8148bc5734b67a5a7b5a3e2187517
Arpit Agrawal <arpitagrawal312@gmail.com>
2020-04-04 21:03:11 +0530

    Create team.js

---
+++ models/team.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Team = sequelize.define('Team', {
+    teamname: DataTypes.STRING,
+    members : [{
+      member: {
+        type: DataTypes.INTEGER,
+        references:{
+          model: 'User',
+          key: 'id'
+        }
+      }
+    }],
+    
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Team.hasMany(models.User, {foreignKey: 'userId'})
+      }
+    },
+    instanceMethods: {
+
+    }
+  })
+  return Team;
+}
