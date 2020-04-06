cc2e856684eaa30eb25ebab56885da8a29d784a2
Shubham <shubhamshahrising@gmail.com>
2020-04-06 11:30:48 +0530

    handling task error pages

---
+++ frontend/src/components/FourOFour.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Block } from '@material-ui/icons'
+import { Link } from '@material-ui/core'
+
+export default () => {
+  return (
+    <div style={ {
+      margin: 'auto',
+      width: '80%',
+      display: 'flex',
+      flexDirection: 'column',
+      fontFamily: 'monospace',
+      textAlign: 'center'
+    } } >
+      <div style={ {
+        display: 'flex',
+        height: '40vh',
+        justifyContent: 'center',
+        alignItems: 'center'
+      } }>
+        <Block style={ {
+          fontSize: 200,
+          color: 'rgb(200,0,50)'
+        } } />
+        <p style={ {
+          fontSize: 40,
+        } }>
+         404 Page not Found
+        </p>
+      </div>
+      <p style={ { fontSize: 25 } }>You may want to visit below pages</p>
+      <div style={ {
+        fontSize: 20,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      } }>
+        <p><Link href='/'>Home</Link></p>
+        <p><Link href='#/tasks/all'>Tasks</Link></p>
+        <p><Link href='#/login'>Login</Link></p>
+      </div>
+    </div>
+  )
+}
