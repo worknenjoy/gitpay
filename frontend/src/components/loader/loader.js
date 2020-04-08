39552dcaa4d8ca62eeca917c3edf9b0cc8a5c851
etfagha oubeid <etfagha@softprofiles.com>
2020-04-08 11:06:40 +0000

    add some style to loader component

--- frontend/src/components/loader/loader.js
+++ frontend/src/components/loader/loader.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
-function Loader () {
-  return <CircularProgress disableShrink size={ 200 } color='secondary' />
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center', // fix me : this property did not  work
+    marginTop: '48vh',
+  },
+  progress: {
+    color: '#009688',
+  }
+})
+
+function CustomizedProgress ({ classes }) {
+  return (
+    <div className={ classes.root }>
+      <CircularProgress className={ classes.progress } disableShrink size={ 60 } />
+    </div>
+  )
 }
-export default Loader
+export default withStyles(styles)(CustomizedProgress)
