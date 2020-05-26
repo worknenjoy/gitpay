871fb22231330370c76ff5b772b55bb2786680da
namandangi <danginaman55@gmail.com>
2020-05-27 02:35:41 +0530

    set  mapping for user roles

--- frontend/src/containers/profile.js
+++ frontend/src/containers/profile.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import Profile from '../components/profile/profile'
 import { fetchPreferences } from '../actions/preferencesActions'
+import { fetchRoles } from '../actions/roleActions'
 import { updateUser, deleteUser } from '../actions/userActions'
 import { fetchOrganizations, createOrganizations } from '../actions/organizationsActions'
 
@@ -9,6 +10,7 @@ const mapStateToProps = (state, ownProps) => {
     logged: state.loggedIn.logged,
     user: state.loggedIn.user,
     preferences: state.preferences,
+    roles: state.roles,
     organizations: state.organizations.organizations,
     completed: state.loggedIn.completed
   }
@@ -17,6 +19,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     fetchPreferences: (userId) => dispatch(fetchPreferences(userId)),
+    fetchRoles: (userId) => dispatch(fetchRoles(userId)),
     fetchOrganizations: (userId) => dispatch(fetchOrganizations(userId)),
     createOrganizations: (org) => dispatch(createOrganizations(org)),
     updateUser: (userId, userData) => dispatch(updateUser(userId, userData)),
