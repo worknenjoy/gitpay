8822516653e29a8c3a9d481b66c3a56b5d9cabc4
Aryan Choudhary <sep2001aryan@yahoo.com>
2020-06-19 16:15:00 +0530

    eslintfix

--- frontend/src/components/Stats/Charts/Barchart.js
+++ frontend/src/components/Stats/Charts/Barchart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 
 const data = [{ name: 'Jan', uv: 400, pv: 2400, amt: 2400 }, { name: 'Feb', uv: 300, pv: 2400, amt: 2400 }, { name: 'Mar', uv: 200, pv: 2400, amt: 2400 }, { name: 'Apr', uv: 100, pv: 2400, amt: 2400 }, { name: 'May', uv: 150, pv: 2400, amt: 2400 }, { name: 'Jun', uv: 400, pv: 2400, amt: 2400 }, { name: 'Jul', uv: 80, pv: 2400, amt: 2400 }, { name: 'Aug', uv: 10, pv: 2400, amt: 2400 }, { name: 'Sep', uv: 200, pv: 2400, amt: 2400 }, { name: 'Oct', uv: 300, pv: 2400, amt: 2400 }, { name: 'Nov', uv: 250, pv: 2400, amt: 2400 }, { name: 'Dec', uv: 220, pv: 2400, amt: 2400 }]
 export default class Barchart extends Component {
