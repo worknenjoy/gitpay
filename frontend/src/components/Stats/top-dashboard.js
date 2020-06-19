50709a7d8abcf4bae16057299018bcea916b9c12
Aryan Choudhary <sep2001aryan@yahoo.com>
2020-06-19 16:28:11 +0530

    eslint fix

--- frontend/src/components/Stats/top-dashboard.js
+++ frontend/src/components/Stats/top-dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import media from '../../styleguide/media'
 
 const Wrapper = styled.div`
