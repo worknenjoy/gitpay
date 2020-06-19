7d86f42577bed1aa34f8121d95ea41389a236720
Aryan Choudhary <sep2001aryan@yahoo.com>
2020-06-19 16:20:06 +0530

    eslint fix

--- frontend/src/components/Stats/InformationCards/Earnings.js
+++ frontend/src/components/Stats/InformationCards/Earnings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import LocalAtmIcon from '@material-ui/icons/LocalAtm'
 
 const Card = styled.div`
