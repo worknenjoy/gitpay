7b85a3a650f8f497b8d768127d823b3792c196b9
Aryan Choudhary <sep2001aryan@yahoo.com>
2020-06-19 16:18:08 +0530

    eslint fix

--- frontend/src/components/Stats/InformationCard.js
+++ frontend/src/components/Stats/InformationCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import AvrageBountyPrice from './InformationCards/AvrageBountyPrice'
 import Bounties from './InformationCards/Bounties'
 import Earnings from './InformationCards/Earnings'
