934da1465186126534496c931041331be9c88f16
Freedom <free.eve@pm.me>
2020-07-11 21:10:53 -0700

    userTasks now finds all users assigns returning associated TaskIds

--- modules/users/userTasks.js
+++ modules/users/userTasks.js
@@ -1,1 +1,19 @@
-// TODO create ednpoint to find number of completed tasks by user
+const models = require('../../models')
+const Promise = require('bluebird')
+
+module.exports = Promise.method(function userTasks (id) {
+  return models.Assign
+    .findAll({
+      attributes: ['TaskId'],
+      where: {
+        userId: id
+      }
+    })
+    .then(assigns => {
+      return assigns
+    }).catch(error => {
+      // eslint-disable-next-line no-console
+      console.log('Error at userTasks', error)
+      throw error
+    })
+})
