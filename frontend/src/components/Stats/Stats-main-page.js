3d1034c465dbe0bf7914a46028a5b82a6f2485d6
sep2001aryan <sep2001aryan@yahoo.com>
2020-08-02 14:09:09 +0530

    added bottom nav to stats page

--- frontend/src/components/Stats/Stats-main-page.js
+++ frontend/src/components/Stats/Stats-main-page.js
@@ -1,13 +1,14 @@
 /* eslint-disable indent */
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import TopBarContiner from '../../containers/topbar'
-import TopDashboard from './top-dashboard'
-import InformationCard from './InformationCard'
-import StackData from './StackData'
-import CurrentMonthStats from './CurrentMonthStats'
-import Barchart from './Charts/Barchart'
-import media from '../../styleguide/media'
+import React, { Component } from "react";
+import styled from "styled-components";
+import TopBarContiner from "../../containers/topbar";
+import TopDashboard from "./top-dashboard";
+import InformationCard from "./InformationCard";
+import StackData from "./StackData";
+import CurrentMonthStats from "./CurrentMonthStats";
+import Barchart from "./Charts/Barchart";
+import media from "../../styleguide/media";
+import Bottom from "../../components/bottom/bottom";
 
 const Wrapper = styled.div`
   margin: 0;
@@ -19,7 +20,7 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
   `}
-`
+`;
 
 const ChartWrapper = styled.div`
   margin: 0;
@@ -30,37 +31,38 @@ const ChartWrapper = styled.div`
   ${media.phone`
     width: 400px;
   `}
-`
+`;
 const StatsPageWrapper = styled.div`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   width: 1150px;
   margin: 0 auto;
   ${media.phone`
     width: 400px;
   `}
-`
+`;
 
 class Stats extends Component {
-    render () {
-        return (
-          <React.Fragment>
-            <div style={ { background: '#FFFFFF' } }>
-              <TopBarContiner />
-              <StatsPageWrapper>
-                <TopDashboard />
-                <Wrapper>
-                  <StackData />
-                  <CurrentMonthStats />
-                </Wrapper>
-                <InformationCard />
-                <ChartWrapper>
-                  <Barchart style={ { width: '400px' } } />
-                </ChartWrapper>
-              </StatsPageWrapper>
-            </div>
-          </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div style={{ background: "#FFFFFF" }}>
+          <TopBarContiner />
+          <StatsPageWrapper>
+            <TopDashboard />
+            <Wrapper>
+              <StackData />
+              <CurrentMonthStats />
+            </Wrapper>
+            <InformationCard />
+            <ChartWrapper>
+              <Barchart style={{ width: "400px" }} />
+            </ChartWrapper>
+          </StatsPageWrapper>
+          <Bottom />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Stats
+export default Stats;
