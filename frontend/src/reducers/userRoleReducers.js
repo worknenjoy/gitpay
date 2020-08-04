01a75405e66632afd06a98e2d7203680bd7282b3
namandangi <danginaman55@gmail.com>
2020-08-05 00:49:18 +0530

    setup reducers for user-roles

---
+++ frontend/src/reducers/userRoleReducers.js
@@ -0,0 +1,28 @@
+import {
+    FETCH_ROLES_REQUESTED,
+    FETCH_ROLES_SUCCESS,
+    FETCH_ROLES_ERROR,
+    UPDATE_ROLE_REQUESTED,
+    UPDATE_ROLE_SUCCESS,
+    UPDATE_ROLE_ERROR
+  } from '../actions/userRoleActions'
+  
+  export const roles = (state = { name: null, label: null, userId: null, id: null, completed: true, error: {} }, action) => {
+    switch (action.type) {
+      case FETCH_ROLES_REQUESTED:
+        return { ...state, completed: action.completed }
+      case FETCH_ROLES_SUCCESS:
+        return { ...state, completed: action.completed, name: action.name, label: action.label, userId: action.userId, id: action.id }
+      case FETCH_ROLES_ERROR:
+        return { ...state, error: action.error, completed: action.completed }
+      case UPDATE_ROLE_REQUESTED:
+        return { ...state, completed: action.completed }
+      case UPDATE_ROLE_SUCCESS:
+        return { ...state, completed: action.completed, name: action.name, label: action.label, userId: action.userId, id: action.id }
+      case UPDATE_ROLE_ERROR:
+        return { ...state, error: action.error, completed: action.completed }
+      default:
+        return state
+    }
+  }
+  
