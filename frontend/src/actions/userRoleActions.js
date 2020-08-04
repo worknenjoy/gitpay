4fa0fd5bf36f9a409909670b3614a89d5ced4d64
namandangi <danginaman55@gmail.com>
2020-08-05 00:45:50 +0530

    setup actions for user-roles

---
+++ frontend/src/actions/userRoleActions.js
@@ -0,0 +1,101 @@
+import axios from 'axios'
+import api from '../consts'
+import { loggedIn } from './loginActions'
+import { validToken } from './helpers'
+import { addNotification } from './notificationActions'
+
+const FETCH_ROLES_REQUESTED = 'FETCH_ROLES_REQUESTED'
+const FETCH_ROLES_SUCCESS = 'FETCH_ROLES_SUCCESS'
+const FETCH_ROLES_ERROR = 'FETCH_ROLES_ERROR'
+const UPDATE_ROLE_REQUESTED = 'UPDATE_ROLE_REQUESTED'
+const UPDATE_ROLE_SUCCESS = 'UPDATE_ROLE_SUCCESS'
+const UPDATE_ROLE_ERROR = 'UPDATE_ROLE_ERROR'
+
+const fetchRolesRequested = () => {
+  return { type: FETCH_ROLES_REQUESTED, completed: false }
+}
+
+const fetchRolesSuccess = (response) => {
+  return { type: FETCH_ROLES_SUCCESS, completed: true, name: response.name, label: response.label, userId: response.userId, id: response.id }
+}
+
+const fetchRolesError = (error) => {
+  return { type: FETCH_ROLES_ERROR, completed: true, error }
+}
+
+const updateRoleRequested = () => {
+  return { type: UPDATE_ROLE_REQUESTED, completed: false }
+}
+
+const updateRoleSuccess = (response) => {
+  return {
+    type: UPDATE_ROLE_SUCCESS, completed: true, name: response.name, label: response.label, userId: response.userId, id: response.id }
+}
+
+const updateRoleError = (error) => {
+  return { type: UPDATE_ROLE_ERROR, completed: true, error: error }
+}
+
+const fetchRoles = () => {
+  validToken()
+  return (dispatch) => {
+    return dispatch(loggedIn()).then(user => {
+      dispatch(fetchRolesRequested())
+      return axios
+        .get(`${api.API_URL}/roles/fetch`)
+        .then(response => {
+          // eslint-disable-next-line no-console
+          // console.log(response.data)
+          return dispatch(fetchRolesSuccess(response.data))
+        })
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          // console.log(error)
+          return dispatch(fetchRolesError(error))
+        })
+    })
+  }
+}
+
+const updateRoles = (rolesData) => {
+  // console.log(rolesData)
+  validToken()
+  return (dispatch) => {
+    return dispatch(loggedIn()).then(user => {
+      dispatch(updateRoleRequested())
+      return axios
+        .put(`${api.API_URL}/roles/update`, {
+          name: rolesData.name
+        })
+        .then(response => {
+          // eslint-disable-next-line no-console
+          // console.log(response)
+          // dispatch(addNotification('user.role.update.success'))
+          dispatch(addNotification('Role Updated Successfully'))
+          return dispatch(fetchRolesSuccess(response.data))
+        })
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          // console.log(error)
+          return dispatch(fetchRolesError(error))
+        })
+    })
+  }
+}
+
+export {
+  FETCH_ROLES_REQUESTED,
+  FETCH_ROLES_SUCCESS,
+  FETCH_ROLES_ERROR,
+  UPDATE_ROLE_REQUESTED,
+  UPDATE_ROLE_SUCCESS,
+  UPDATE_ROLE_ERROR,
+  fetchRolesRequested,
+  fetchRolesSuccess,
+  fetchRolesError,
+  fetchRoles,
+  updateRoleRequested,
+  updateRoleSuccess,
+  updateRoleError,
+  updateRoles
+}
