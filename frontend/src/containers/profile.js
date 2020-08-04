ab5b8c31cedc9ab0f5ed11d2c8c0a83e9ed572d0
namandangi <danginaman55@gmail.com>
2020-08-05 00:47:53 +0530

    setup container for user-roles

--- frontend/src/containers/profile.js
+++ frontend/src/containers/profile.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux'
 import Profile from '../components/profile/profile'
 import { fetchPreferences } from '../actions/preferencesActions'
+import { fetchRoles, updateRoles } from '../actions/userRoleActions'
 import { updateUser, deleteUser } from '../actions/userActions'
 import { fetchOrganizations, createOrganizations } from '../actions/organizationsActions'
+import { addNotification, closeNotification } from '../actions/notificationActions'
 
 const mapStateToProps = (state, ownProps) => {
   return {
     logged: state.loggedIn.logged,
     user: state.loggedIn.user,
     preferences: state.preferences,
+    roles: state.roles,
     organizations: state.organizations.organizations,
     completed: state.loggedIn.completed
   }
@@ -17,10 +20,14 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     fetchPreferences: (userId) => dispatch(fetchPreferences(userId)),
+    fetchRoles: dispatch(fetchRoles()),
+    updateRoles: (rolesData) => dispatch(updateRoles(rolesData)),
     fetchOrganizations: (userId) => dispatch(fetchOrganizations(userId)),
     createOrganizations: (org) => dispatch(createOrganizations(org)),
     updateUser: (userId, userData) => dispatch(updateUser(userId, userData)),
-    deleteUser: (user) => dispatch(deleteUser(user))
+    deleteUser: (user) => dispatch(deleteUser(user)),
+    addNotification: (message) => dispatch(addNotification(message)),
+    closeNotification: (message) => dispatch(closeNotification(message))
   }
 }
 
