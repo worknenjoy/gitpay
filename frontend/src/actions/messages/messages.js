97ba7df7fd7d0f7a417f3705d45de3759241dc7c
Alexandre Magno <alexanmtz@gmail.com>
2020-08-05 20:18:43 +0200

    starting to user a definition messages for notifications in order to generate the right translations

---
+++ frontend/src/actions/messages/messages.js
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl'
+
+const messages = defineMessages({
+  notificationLoginSuccessfull: {
+    id: 'user.login.successfull',
+    defaultMessage: 'You successfully logged in'
+  }
+})
+
+export default messages
