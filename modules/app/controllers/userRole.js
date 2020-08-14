b0b0c2ca3012b41523949220a6b9d0cfb450ef06
namandangi <danginaman55@gmail.com>
2020-08-14 22:52:23 +0530

    clean user role controllers

--- modules/app/controllers/userRole.js
+++ modules/app/controllers/userRole.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 const UserRoles = require('../../userRoles')
 /*
  * GET @ /roles/fetch
@@ -8,13 +10,11 @@ exports.fetchUserRole = async (req, res) => {
     req.body.userId = req.user.id
     let doc = await UserRoles.userRoleFetch(req.body)
     if (doc) {
-      // eslint-disable-next-line no-console
       console.log('found role', doc)
       return res.status(200).send(doc)
     }
   }
   catch (err) {
-    // eslint-disable-next-line no-console
     console.log(err)
     return res.status(500).send(false)
   }
@@ -28,38 +28,15 @@ exports.createUserRole = async (req, res) => {
     req.body.userId = req.user.id
     let doc = await UserRoles.userRoleCreate(req.body)
     if (doc) {
-      // eslint-disable-next-line no-console
       console.log('role created', doc)
       return res.status(201).send(doc)
     }
   }
   catch (err) {
-    // eslint-disable-next-line no-console
     console.log(err)
     return res.status(500).send(false)
   }
 }
-// /*
-//  * PUT @ /roles/update
-//  *
-//  */
-// exports.updateUserRole = async (req, res) => {
-//   try {
-//     req.body.userId = req.user.id
-//     let doc = await Role.userRoleUpdate(req.body)
-//     if (doc) {
-//       // eslint-disable-next-line no-console
-//       console.log('role updated', doc)
-//       return res.status(201).send(doc)
-//     }
-//   }
-//   catch (err) {
-//     // eslint-disable-next-line no-console
-//     console.log(err)
-//     return res.status(500).send(false)
-//   }
-// }
-
 /*
  * DELETE @ /roles/delete
  *
@@ -69,13 +46,11 @@ exports.deleteUserRole = async (req, res) => {
     req.body.userId = req.user.id
     let doc = await UserRoles.userRoleDelete(req.body)
     if (doc) {
-      // eslint-disable-next-line no-console
       console.log('role deleted')
       return res.status(200).send({ 'msg': 'ok' })
     }
   }
   catch (err) {
-    // eslint-disable-next-line no-console
     console.log(err)
     return res.status(500).send(false)
   }
