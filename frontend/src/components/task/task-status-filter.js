565855df5e5c6abd3cceab40c46f2fdaa2df674f
Simran Chaudhary <Simran.Chaudhary@walmart.com>
2020-08-29 15:14:41 +0530

    fixed selected clip style with linting fixes

--- frontend/src/components/task/task-status-filter.js
+++ frontend/src/components/task/task-status-filter.js
@@ -1,139 +1,140 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { messages } from "./messages/task-messages";
-import { injectIntl, FormattedMessage } from "react-intl";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+import { messages } from './messages/task-messages'
+import { injectIntl, FormattedMessage } from 'react-intl'
 
-import { withStyles, Chip } from "@material-ui/core";
+import {
+  withStyles,
+  Chip
+} from '@material-ui/core'
 
 const styles = theme => ({
   selected: {
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    "&:focus": {
+    '&:focus': {
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.primary.contrastText
     }
   }
-});
+})
 
-const statuses = ["open", "in_progress", "closed"];
-const additionalStatuses = ["issuesWithBounties", "contribution"];
+const statuses = ['open', 'in_progress', 'closed']
+const additionalStatuses = ['issuesWithBounties', 'contribution']
 
 class TaskStatusFilter extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      selected: "all",
+      selected: 'all',
       additionalSelected: null
-    };
+    }
   }
 
   static propTypes = {
     classes: PropTypes.object.isRequired,
     onFilter: PropTypes.func
-  };
+  }
 
   componentDidMount() {
-    let pathName = this.props.history.location.pathname;
-    this.handleFromUrl(pathName);
+    let pathName = this.props.history.location.pathname
+    this.handleFromUrl(pathName)
   }
 
   handleFromUrl = value => {
     switch (value) {
-      case "/tasks/open":
-        this.props.onFilter("status", "open");
-        this.setState({ selected: "open" });
-        break;
-      case "/tasks/progress":
-        this.props.onFilter("status", "in_progress");
-        this.setState({ selected: "in_progress" });
-        break;
-      case "/tasks/finished":
-        this.props.onFilter("status", "closed");
-        this.setState({ selected: "closed" });
-        break;
-      case "/tasks/with-bounties":
-        this.props.onFilter("status", "issuesWithBounties");
-        this.setState({ additionalSelected: "issuesWithBounties" });
-        break;
-      case "/tasks/contribution":
-        this.props.onFilter("status", "contribution");
-        this.setState({ additionalSelected: "contribution" });
-        break;
+      case '/tasks/open':
+        this.props.onFilter('status', 'open')
+        this.setState({ selected: 'open' })
+        break
+      case '/tasks/progress':
+        this.props.onFilter('status', 'in_progress')
+        this.setState({ selected: 'in_progress' })
+        break
+      case '/tasks/finished':
+        this.props.onFilter('status', 'closed')
+        this.setState({ selected: 'closed' })
+        break
+      case '/tasks/with-bounties':
+        this.props.onFilter('status', 'issuesWithBounties')
+        this.setState({ additionalSelected: 'issuesWithBounties' })
+        break
+      case '/tasks/contribution':
+        this.props.onFilter('status', 'contribution')
+        this.setState({ additionalSelected: 'contribution' })
+        break
       default:
-        this.props.onFilter();
+        this.props.onFilter()
     }
-  };
+  }
 
   handleListItemClick = value => {
     switch (value) {
-      case "open":
-        this.props.history.push("/tasks/open");
-        this.props.onFilter("status", value, this.state.additionalSelected);
-        break;
-      case "in_progress":
-        this.props.history.push("/tasks/progress");
-        this.props.onFilter("status", value, this.state.additionalSelected);
-        break;
-      case "closed":
-        this.props.history.push("/tasks/finished");
-        this.props.onFilter("status", value, this.state.additionalSelected);
-        break;
+      case 'open':
+        this.props.history.push('/tasks/open')
+        this.props.onFilter('status', value, this.state.additionalSelected)
+        break
+      case 'in_progress':
+        this.props.history.push('/tasks/progress')
+        this.props.onFilter('status', value, this.state.additionalSelected)
+        break
+      case 'closed':
+        this.props.history.push('/tasks/finished')
+        this.props.onFilter('status', value, this.state.additionalSelected)
+        break
       default:
-        this.props.onFilter();
+        this.props.onFilter()
     }
-    this.setState({ selected: value });
-  };
+    this.setState({ selected: value })
+  }
 
   handleListAdditionalStatusesClick = value => {
     switch (value) {
-      case "issuesWithBounties":
-        this.props.history.push("/tasks/with-bounties");
-        this.props.onFilter("status", this.state.selected, value); // passing value as third parameter to consider as additional
-        break;
-      case "contribution":
-        this.props.history.push("/tasks/contribution");
-        this.props.onFilter("status", this.state.selected, value); // passing value as third parameter to consider as additional
-        break;
+      case 'issuesWithBounties':
+        this.props.history.push('/tasks/with-bounties')
+        this.props.onFilter('status', this.state.selected, value) // passing value as third parameter to consider as additional
+        break
+      case 'contribution':
+        this.props.history.push('/tasks/contribution')
+        this.props.onFilter('status', this.state.selected, value) // passing value as third parameter to consider as additional
+        break
       default:
-        this.props.onFilter();
+        this.props.onFilter()
     }
-    this.setState({ additionalSelected: value });
-  };
+    this.setState({ additionalSelected: value })
+  }
 
   handleClickAll = () => {
-    this.props.history.push("/tasks/all");
-    this.props.onFilter();
-    this.setState({ selected: "all", additionalSelected: null });
-  };
+    this.props.history.push('/tasks/all')
+    this.props.onFilter()
+    this.setState({ selected: 'all', additionalSelected: null })
+  }
 
   statusesDisplay = status => {
     const possibles = {
       open: this.props.intl.formatMessage(messages.openStatus),
       in_progress: this.props.intl.formatMessage(messages.inProgressStatus),
       closed: this.props.intl.formatMessage(messages.closed)
-    };
-    return possibles[status];
-  };
+    }
+    return possibles[status]
+  }
 
   additionalStatusDisplay = status => {
     const additional = {
-      issuesWithBounties: this.props.intl.formatMessage(
-        messages.issuesWithBounties
-      ),
+      issuesWithBounties: this.props.intl.formatMessage(messages.issuesWithBounties),
       contribution: this.props.intl.formatMessage(messages.contribution)
-    };
+    }
 
-    return additional[status];
-  };
+    return additional[status]
+  }
 
   render() {
-    const { selected, additionalSelected } = this.state;
-    const { classes } = this.props;
+    const { selected, additionalSelected } = this.state
+    const { classes } = this.props
     return (
       <div>
-        <FormattedMessage id="task.status.filter.all" defaultMessage="All">
+        <FormattedMessage id='task.status.filter.all' defaultMessage='All'>
           {msg => (
             <Chip
               style={{ marginRight: 10 }}
@@ -141,7 +142,7 @@ class TaskStatusFilter extends Component {
               clickable
               key={0}
               label={msg}
-              className={selected === "all" ? classes.selected : {}}
+              className={selected === 'all' ? classes.selected : {}}
             />
           )}
         </FormattedMessage>
@@ -155,29 +156,29 @@ class TaskStatusFilter extends Component {
             className={selected === status ? classes.selected : {}}
           />
         ))}
-        <span
-          style={{
-            flexGrow: 1,
-            flexBasis: "auto",
-            margin: ".25em 0",
-            padding: "5px 0.3em",
-            borderLeft: "1px solid #CCC",
-            backgroundColor: "#FFF"
-          }}
-        />
-        {additionalStatuses.map((status, index) => (
-          <Chip
-            style={{ marginRight: 10 }}
-            onClick={() => this.handleListAdditionalStatusesClick(status)}
-            clickable
-            key={index + 1}
-            label={this.additionalStatusDisplay(status)}
-            className={additionalSelected === status ? classes.selected : {}}
-          />
-        ))}
+        <span style={{
+          flexGrow: 1,
+          flexBasis: 'auto',
+          margin: '.25em 0',
+          padding: '5px 0.3em',
+          borderLeft: '1px solid #CCC',
+          backgroundColor: '#FFF',
+        }} />
+        {
+          additionalStatuses.map((status, index) => (
+            <Chip
+              style={{ marginRight: 10 }}
+              onClick={() => this.handleListAdditionalStatusesClick(status)}
+              clickable
+              key={index + 1}
+              label={this.additionalStatusDisplay(status)}
+              className={additionalSelected === status ? classes.selected : {}}
+            />
+          ))
+        }
       </div>
-    );
+    )
   }
 }
 
-export default injectIntl(withRouter(withStyles(styles)(TaskStatusFilter)));
+export default injectIntl(withRouter(withStyles(styles)(TaskStatusFilter)))
