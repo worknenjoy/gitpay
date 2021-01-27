1db8e09c63e67437ea128043f113dbc26625e807
Alexandre Magno <alexanmtz@gmail.com>
2021-01-27 20:04:20 +0100

    changing private query to fix null values in private field

--- modules/tasks/taskSearch.js
+++ modules/tasks/taskSearch.js
@@ -2,7 +2,14 @@ const Promise = require('bluebird')
 const models = require('../../models')
 
 module.exports = Promise.method(function taskSearch (projectId) {
+<<<<<<< HEAD
   let query = { private: false }
+=======
+  let query = { $or: [
+    { private: null },
+    { private: false }
+  ] }
+>>>>>>> 23dcce1... fixing private query
   return models.Task
     .findAll(
       {
