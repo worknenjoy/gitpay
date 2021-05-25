8ef44d7bce09ff6336d36bd595d4c00c8b7b4e2a
Victor Alessander <alessander@protonmail.com>
2021-05-25 06:17:58 -0300

    Issue#801 (#830)
    * issue #801
    
    * issue #801
    
    * Issue #801
    
    Co-authored-by: Victor Alessander <victor.alessander@linksolutions.com.br>
    Co-authored-by: Victor Alessander <victor.rocha@invillia.com>

--- modules/tasks/updateTaskSolution.js
+++ modules/tasks/updateTaskSolution.js
@@ -25,7 +25,7 @@ module.exports = Promise.method(function updateTaskSolution (taskSolution, taskS
           taskId: taskSolution.taskId
         }).then(response => {
           if (response.isAuthorOfPR && response.isConnectedToGitHub && response.isIssueClosed && response.isPRMerged) {
-            if (!taskData.dataValues.paid) {
+            if (!taskData.dataValues.paid && !taskData.dataValues.transfer_id) {
               taskPayment({ taskId: taskData.dataValues.id, value: taskData.dataValues.value })
             }
           }
