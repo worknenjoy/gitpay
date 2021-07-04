b54c2024f1c0c60931f28f6a58937e71503c20a8
Alexandre Magno <alexanmtz@gmail.com>
2021-07-04 17:14:56 +0200

    fixing status order update from paid to succeeded

--- modules/app/controllers/webhook.js
+++ modules/app/controllers/webhook.js
@@ -531,7 +531,7 @@ exports.updateWebhook = (req, res) => {
       case 'invoice.updated':
         return models.Order.update(
           {
-            paid: event.data.object.status === 'paid' || 'succeeded',
+            paid: event.data.object.status === 'paid' ? 'succeeded' : false,
             status: event.data.object.status,
             source: event.data.object.charge
           },
