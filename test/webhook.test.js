a292b160c814eccf3ec1e95585ed834985e4a4c2
Alexandre Magno <alexanmtz@gmail.com>
2021-07-05 11:36:18 +0200

    Fixing status order update from paid to succeeded (#838)
    * fixing status order update from paid to succeeded
    
    * fixing tests on update invoice order

--- test/webhook.test.js
+++ test/webhook.test.js
@@ -471,6 +471,7 @@ describe('webhooks', () => {
                       include: [models.Task]
                     }).then(orderFinal => {
                       expect(orderFinal.dataValues.paid).to.equal(true)
+                      expect(orderFinal.dataValues.status).to.equal('succeeded')
                       expect(orderFinal.dataValues.source).to.equal('ch_1IlAjBBrSjgsps2DLjTdMXwJ')
                       expect(orderFinal.dataValues.Task.dataValues.url).to.equal(github_url)
                       done()
