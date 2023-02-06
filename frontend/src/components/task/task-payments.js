32793e980acaf407d6cebf9b053f0df062e29878
alexandremagno <alz@cluedin.com>
2023-02-06 10:29:25 -0300

    lint fixes

--- frontend/src/components/task/task-payments.js
+++ frontend/src/components/task/task-payments.js
@@ -21,48 +21,51 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function TaskPayments({ orders }) {
+export default function TaskPayments ({ orders }) {
   const classes = useStyles()
   if (!orders) return <div />
   return orders && orders.length > 0 && (
-    <List dense className={classes.root}>
-      <Typography variant='caption' align='center' style={{ display: 'inline-block', width: '100%', marginBottom: 10 }}>
+    <List dense className={ classes.root }>
+      <Typography variant='caption' align='center' style={ { display: 'inline-block', width: '100%', marginBottom: 10 } }>
         Paid by
       </Typography>
-      {orders && orders.map((order) => {
+      { orders && orders.map((order) => {
         return (
-          <ListItem key={order.id}>
-            <ListItemAvatar style={{ minWidth: 34 }}>
-              {order.User
-                ? <Avatar
-                  className={classes.small}
-                  alt={
-                    `${order.User && (order.User.name || order.User.username) || 'Anonymous'}`
-                  }
-                  src={order.User && order.User.profile_url}
-                /> : <Avatar className={classes.small} />
+          <ListItem key={ order.id }>
+            <ListItemAvatar style={ { minWidth: 34 } }>
+              { order.User
+                ?
+                  <Avatar
+                    className={ classes.small }
+                    alt={
+                      `${order.User && (order.User.name || order.User.username) || 'Anonymous'}`
+                    }
+                    src={ order.User && order.User.profile_url }
+                  />
+                  :
+                  <Avatar className={ classes.small } />
               }
             </ListItemAvatar>
             <ListItemText primary={
               <React.Fragment>
-                {order.User
-                  ? <Typography variant='body2'>{order.User && order.User.name || order.User.username || 'Anonymous'}</Typography>
+                { order.User
+                  ? <Typography variant='body2'>{ order.User && order.User.name || order.User.username || 'Anonymous' }</Typography>
                   : <Typography variant='body2'>
                     <FormattedMessage id='task.payment.order.nouser' defaultMessage='Some user' />
                   </Typography>
                 }
-                <Typography color='textSecondary' variant='caption'> {MomentComponent(order.updatedAt).fromNow()}</Typography>
-                <Typography variant='caption'> with <i>{order.provider === 'paypal' ? 'Paypal' : 'Credit Card'}</i></Typography>
+                <Typography color='textSecondary' variant='caption'> { MomentComponent(order.updatedAt).fromNow() }</Typography>
+                <Typography variant='caption'> with <i>{ order.provider === 'paypal' ? 'Paypal' : 'Credit Card' }</i></Typography>
               </React.Fragment>
             } />
             <ListItemSecondaryAction>
               <Typography variant='caption'>
-                <strong>${order.amount}</strong>
+                <strong>${ order.amount }</strong>
               </Typography>
             </ListItemSecondaryAction>
           </ListItem>
         )
-      })}
+      }) }
     </List>
   )
 }
