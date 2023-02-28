62c4a65001f6fecdc73241d825d58df75bab2cc2
alexandremagno <alz@cluedin.com>
2023-02-28 20:43:56 -0300

    fixing site map builder webpack config

--- frontend/sitemap-builder.js
+++ frontend/sitemap-builder.js
@@ -1,3 +1,11 @@
+require('@babel/register')({
+  ignore: [
+    'node_modules/**/*'
+  ],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+})
+
 const router = require('./src/main/routes-sitemap').default
 const Sitemap = require('react-router-sitemap').default
 
