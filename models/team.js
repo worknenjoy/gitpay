412e6fedbf4c7f6e55e214e720e42386054f2e24
Daniel-olaO <88808467+Daniel-olaO@users.noreply.github.com>
2023-04-26 08:36:32 -0400

    Created Team schema

---
+++ models/team.js
@@ -0,0 +1,12 @@
+module.exports = function (sequelize, DataTypes) {
+  const Team = sequelize.define('Team', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        AllowNull: false
+    },
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    timestamps: true,
+  });
+}
