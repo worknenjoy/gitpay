696297b6e5660629902772b43602b59ec9b1653d
Alexandre Magno <alexanmtz@gmail.com>
2024-01-02 17:06:16 +0100

    adding one more attribute to the transfer model

---
+++ models/transfer.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Transfer = sequelize.define('Transfer', {
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'pending'
+    },
+    value: DataTypes.DECIMAL,
+    transfer_id: DataTypes.STRING,
+    transfer_method: DataTypes.STRING,
+    taskId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tasks',
+        key: 'id'
+      },
+      allowNull: false,
+    }
+  }, {
+
+  })
+
+  Transfer.associate = function (models) {
+    Transfer.belongsTo(models.Task)
+  }
+}
