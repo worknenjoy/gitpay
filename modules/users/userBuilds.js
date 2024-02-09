5111f82f91de9695584ec67b4ad866539a954bb1
Alexandre Magno <alexanmtz@gmail.com>
2024-02-09 20:08:30 +0100

    fix: fixing Github login for new users

--- modules/users/userBuilds.js
+++ modules/users/userBuilds.js
@@ -3,7 +3,10 @@ const Sendmail = require('../mail/mail')
 const models = require('../../models')
 
 module.exports = Promise.method(function userBuilds (userParameters) {
-  userParameters.password = models.User.generateHash(userParameters.password)
+  if(userParameters.password !== userParameters.confirmPassword) return false
+  if(userParameters.password) {
+    userParameters.password = models.User.generateHash(userParameters.password)
+  }
   userParameters.activation_token = models.User.generateToken()
   userParameters.email_verified = false
   if (!userParameters.email && !userParameters.password && !userParameters.confirmPassword) return false
@@ -14,7 +17,9 @@ module.exports = Promise.method(function userBuilds (userParameters) {
     .then(data => {
       if (data && data.dataValues && data.dataValues.id) {
         const { id, name, activation_token } = data.dataValues
-        Sendmail.success(data.dataValues, 'Activate your account', `<p>Hi ${name || 'Gitpay user'},</p><p>Click <a href="${process.env.FRONTEND_HOST}/#/activate/user/${id}/token/${activation_token}">here</a> to activate your account.</p>`)
+        if(!userParameters.provider) {
+          Sendmail.success(data.dataValues, 'Activate your account', `<p>Hi ${name || 'Gitpay user'},</p><p>Click <a href="${process.env.FRONTEND_HOST}/#/activate/user/${id}/token/${activation_token}">here</a> to activate your account.</p>`)
+        }
       }
       return data
     }).catch(error => {
