1262fb6779fed74c54167e382c229ac5750b6553
Macaulay Precious <87583799+Precious-Macaulay@users.noreply.github.com>
2024-02-20 15:55:43 +0100

    Update frontend/src/components/task/task-table.js
    Co-authored-by: Alexandre Magno <alexanmtz@gmail.com>

--- frontend/src/components/task/task-table.js
+++ frontend/src/components/task/task-table.js
@@ -249,11 +249,7 @@ class CustomPaginationActionsTable extends React.Component {
 
   render () {
     const { classes, tasks } = this.props
-    const rowsPerPage = this.state.rowsPerPage;
-    const page = this.state.page;
-    const sortedBy = this.state.sortedBy;
-    const sortDirection = this.state.sortDirection;
-    const sortedData = this.state.sortedData;
+    const  { rowsPerPage, page, sortedBy, sortDirection, sortedData  } = this.state;
 
     const emptyRows = sortedData.length ? rowsPerPage - Math.min(rowsPerPage, sortedData.length - page * rowsPerPage) : 0
 
