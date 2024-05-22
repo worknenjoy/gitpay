16dfdf1a91172b6a403cd5fb2f6dcb77c4d60841
Alexandre Magno <alexanmtz@gmail.com>
2024-05-22 18:33:26 +0200

    adding task header metadata for column information about a table

---
+++ frontend/src/components/task/task-header-metadata.jsx
@@ -0,0 +1,16 @@
+export const tableHeaderDefault = {
+    "task.table.head.task": { sortable: true, numeric: false, dataBaseKey: "title", label: 'Issue' },
+    "task.table.head.status": { sortable: true, numeric: false, dataBaseKey: "status", label: 'Status'},
+    "task.table.head.project": { sortable: true, numeric: false, dataBaseKey: "Project.name", label: 'Project'},
+    "task.table.head.value": { sortable: true, numeric: true, dataBaseKey: "value", label: 'Value'},
+    "task.table.head.labels": { sortable: true, numeric: false, dataBaseKey: "Labels", label: 'Labels'},
+    "task.table.head.createdAt": { sortable: true, numeric: false, dataBaseKey: "createdAt", label: 'Created'},
+  }
+  
+  export const tableHeaderWithProject = {
+    "task.table.head.task": { sortable: true, numeric: false, dataBaseKey: "title", label: 'Issue' },
+    "task.table.head.status": { sortable: true, numeric: false, dataBaseKey: "status", label: 'Status'},
+    "task.table.head.value": { sortable: true, numeric: true, dataBaseKey: "value", label: 'Value'},
+    "task.table.head.labels": { sortable: true, numeric: false, dataBaseKey: "Labels", label: 'Labels'},
+    "task.table.head.createdAt": { sortable: true, numeric: false, dataBaseKey: "createdAt", label: 'Created'},
+  }
