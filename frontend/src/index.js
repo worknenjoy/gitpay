eb3e863f7a8270df09b65e2477bce0ea91583308
Anshgrover23 <168731971+Anshgrover23@users.noreply.github.com>
2024-06-17 15:00:39 +0000

    done with requested errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-deprecated */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './main/app';
 
 ReactDOM.render(
-    <App />,
+  <App />,
     document.getElementById('app')
   )
