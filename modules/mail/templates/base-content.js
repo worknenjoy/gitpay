d3339108ce65c45f1396f9d6f258a5a1262acfdf
Alexandre Magno <alexanmtz@gmail.com>
2024-07-14 12:07:54 +0200

    fixing issue with undefined content

--- modules/mail/templates/base-content.js
+++ modules/mail/templates/base-content.js
@@ -1,9 +1,9 @@
-module.exports.baseContentEmailTemplate = (intro, content) => `
+module.exports.baseContentEmailTemplate = (intro, content = '') => `
  <!-- START MAIN CONTENT AREA -->
               <tr>
                 <td class="wrapper" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; box-sizing: border-box; padding: 24px;" valign="top">
                   <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">${intro}</p>
-                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">${content || ''}</p>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">${content}</p>
                 </td>
               </tr>
 
