ff963307b70ae69965e1479effeb296a53782fd1
J_Dheeraj@4103 <69622080+Dheeraj4103@users.noreply.github.com>
2024-08-23 17:49:52 +0530

    updated node-sass to sass in package.json (#1118)
    * updated node-sass to sass in package.json
    
    * converted class component to functional component in skill.js

--- frontend/src/components/profile/skill.js
+++ frontend/src/components/profile/skill.js
@@ -1,39 +1,30 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-import { injectIntl } from 'react-intl'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { injectIntl } from "react-intl";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-import {
-  Grid,
-  Typography,
-  Checkbox,
-  Avatar,
-} from '@material-ui/core'
-import FolderIcon from '@material-ui/icons/Folder'
+import { Grid, Typography, Checkbox, Avatar } from "@material-ui/core";
+import FolderIcon from "@material-ui/icons/Folder";
 
-class Skill extends Component {
-  render () {
-    const { classes, title } = this.props
-
-    return (
-      <Grid container direction='row' alignItems='center' xs={ 6 }>
-        <Grid item xs={ 2 }>
-          <Avatar className={ classNames(classes.avatar, classes.bigAvatar) }>
-            <FolderIcon />
-          </Avatar>
-        </Grid>
-        <Grid item xs={ 6 }>
-          <Typography variant='body1' color='default'>
-            { title }
-          </Typography>
-        </Grid>
-        <Grid item xs={ 4 } alignItems='flex-end'>
-          <Checkbox onClick={ this.props.onClick } checked={ this.props.isSelected ? 'checked' : '' } />
-        </Grid>
+function Skill({ classes, title, onClick, isSelected }) {
+  return (
+    <Grid container direction="row" alignItems="center" xs={6}>
+      <Grid item xs={2}>
+        <Avatar className={classNames(classes.avatar, classes.bigAvatar)}>
+          <FolderIcon />
+        </Avatar>
       </Grid>
-    )
-  }
+      <Grid item xs={6}>
+        <Typography variant="body1" color="default">
+          {title}
+        </Typography>
+      </Grid>
+      <Grid item xs={4} alignItems="flex-end">
+        <Checkbox onClick={onClick} checked={isSelected ? "checked" : ""} />
+      </Grid>
+    </Grid>
+  );
 }
 
 Skill.propTypes = {
@@ -42,7 +33,7 @@ Skill.propTypes = {
   language: PropTypes.string,
   title: PropTypes.string,
   onClick: PropTypes.func.isRequired,
-  isSelected: PropTypes.bool
-}
+  isSelected: PropTypes.bool,
+};
 
-export default injectIntl(withRouter(Skill))
+export default injectIntl(withRouter(Skill));
