2ea1e8c780c762393107576b039f93597ccf7c74
Alexandre Magno <alexanmtz@gmail.com>
2024-10-30 19:38:16 +0100

    actions menu components on design library

---
+++ frontend/src/components/design-library/molecules/actions-menu/actions-menu.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { IconButton, Menu, MenuItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { MoreVert as MoreIcon, Visibility as VisibilityIcon } from '@material-ui/icons';
+
+
+export const ActionsMenu = (props) => {
+  const { actions, ...rest } = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+
+  const handleMoreButton = (e) => {
+    e.preventDefault()
+    console.log('More button clicked')
+    setAnchorEl({ anchorEl: e.currentTarget });
+  }
+
+  return (
+    <div className="actions-menu" {...rest}>
+      <IconButton onClick={handleMoreButton}>
+        <MoreIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={anchorEl}
+        onClose={() => setAnchorEl({ anchorEl: null })}
+      >
+        {actions.map((action, index) => (
+          <MenuItem key={index} onClick={action.onClick} style={{ display: 'flex', alignItems: 'center' }}>
+            <ListItemIcon>
+              <VisibilityIcon />
+            </ListItemIcon>
+            <ListItemText primary={action.children} />
+          </MenuItem>
+        ))}
+       
+      </Menu>
+    </div>
+  );
+}
+
+export default ActionsMenu;
