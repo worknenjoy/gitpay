30dbdd0d56210fa113bc585bc6f08f6cbc475748
Alexandre Magno <alexanmtz@gmail.com>
2024-11-03 00:23:12 +0100

    adding alert as a component to display on task solution and account verification function

---
+++ frontend/src/utils/valid-account.tsx
@@ -0,0 +1,9 @@
+export const validAccount = (user, account) => {
+  if (!user.account_id) {
+    return false
+  } else if (account?.data?.requirements?.currently_due?.length > 0) {
+    return false
+  } else {
+    return true
+  }
+}
