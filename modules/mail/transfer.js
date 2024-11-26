43099a2cbda3bb07aee1b7a1767c0d1c2cb14e8b
Alexandre Magno <alexanmtz@gmail.com>
2024-11-26 10:21:16 +0100

    fixing email template for transfers

--- modules/mail/transfer.js
+++ modules/mail/transfer.js
@@ -2,7 +2,7 @@ const Signatures = require('./content')
 const request = require('./request')
 const constants = require('./constants')
 const i18n = require('i18n')
-const emailTemplate = require('./templates/main-content')
+const emailTemplate = require('./templates/base-content')
 
 const TransferMail = {
   success: (to, task, value) => {},
