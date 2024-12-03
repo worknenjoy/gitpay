c34d6291ddafa9f6bbf5cf8cb1509d80cbfa2f4e
Alexandre Magno <alexanmtz@gmail.com>
2024-12-03 22:56:51 +0100

    new task solution components

---
+++ frontend/src/components/design-library/molecules/requirements-list/requirement-list.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core'
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded'
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded'
+import { makeStyles } from '@material-ui/core/styles'
+import ReactPlaceholder from 'react-placeholder'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+}))
+
+const CustomPlaceholder = () => <CircularProgress size={24} />
+const SendSolutionRequirements = ({ requirements, completed }) => {
+  const classes = useStyles()
+
+  return (
+    <List className={ classes.root }>
+      { requirements.map((requirement, index) => (
+        <ListItem>
+          <ListItemIcon style={ { color: 'black' } }>
+            <ReactPlaceholder showLoadingAnimation ready={ completed } customPlaceholder={ <CustomPlaceholder /> }>
+              { requirement.done ? <CheckCircleRoundedIcon color='primary' /> : <CancelRoundedIcon /> }
+            </ReactPlaceholder>
+          </ListItemIcon>
+          <ListItemText primary={requirement.label} />
+      </ListItem>
+      )) }
+    </List>
+  )
+}
+
+export default SendSolutionRequirements
---
+++ frontend/src/components/task/send-solution-requirements.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import RequirementList from '../design-library/molecules/requirements-list/requirement-list'
+
+const SendSolutionRequirements = ({ completed, isConnectedToGitHub, isAuthorOfPR, isPRMerged, isIssueClosed, hasIssueReference }) => {
+
+  const requirements = [
+    { done: isConnectedToGitHub, label: "You're connected to GitHub" },
+    { done: isAuthorOfPR, label: "You're the author of this Pull Request on GitHub" },
+    { done: isPRMerged, label: 'The Pull Request / Merge Request was merged' },
+    { done: isIssueClosed, label: 'The issue is closed on GitHub' },
+    { done: hasIssueReference, label: 'The issue is referenced on the PR' }
+  ]
+
+  return (
+    <RequirementList requirements={requirements} completed={completed} />
+  )
+}
+
+export default SendSolutionRequirements
