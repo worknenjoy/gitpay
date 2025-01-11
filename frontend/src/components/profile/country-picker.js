88e7b53da82cc4b6547a059a7b2683da6ea70775
Alexandre Magno <alexanmtz@gmail.com>
2025-01-11 18:14:02 -0300

    fixing dialog country picker with credits for images

--- frontend/src/components/profile/country-picker.js
+++ frontend/src/components/profile/country-picker.js
@@ -104,7 +104,12 @@ class CountryPicker extends Component {
               { getCountryButtons() }
             </div>
           </DialogContent>
-          <DialogActions>
+          <DialogActions alignItems='space-evenly'>
+            <DialogContent id='alert-dialog-footer'>
+              <DialogContentText>
+                Icons made by <a href='http://www.freepik.com/' title='Freepik'>Freepik</a>, <a href="https://www.flaticon.com/free-icons/pakistan" title="pakistan icons">Pakistan icons created by Roundicons - Flaticon</a>,  <a href="https://www.flaticon.com/free-icons/turkey" title="turkey icons">Turkey icons created by IconsBox - Flaticon</a> and <a href="https://www.flaticon.com/free-icons/flags" title="flags icons">Icon.doit - Flaticon</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank' rel="noreferrer">CC 3.0 BY</a>
+              </DialogContentText>
+            </DialogContent>
             <Button onClick={ (e) => this.props.onClose(e, {
               country: null,
               code: null,
@@ -116,17 +121,12 @@ class CountryPicker extends Component {
               country: this.state.currentCountryLabel,
               code: this.state.currentCountryCode,
               image: this.state.currentCountryImage
-            }) } size='large' color='secondary' autoFocus>
+            }) } size='large' color='secondary' autoFocus style={{
+              minWidth: 'auto'
+            }}>
               Choose { this.state.currentCountryLabel }
             </Button>
           </DialogActions>
-          <DialogContent>
-            <DialogContentText id='alert-dialog-footer'>
-              <div>Icons made by <a href='http://www.freepik.com/' title='Freepik'>Freepik</a> and <a href="https://www.flaticon.com/free-icons/flags" title="flags icons">Icon.doit - Flaticon</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank' rel="noreferrer">CC 3.0 BY</a></div>
-              <div><a href="https://www.flaticon.com/free-icons/pakistan" title="pakistan icons">Pakistan icons created by Roundicons - Flaticon</a></div>
-              <div><a href="https://www.flaticon.com/free-icons/turkey" title="turkey icons">Turkey icons created by IconsBox - Flaticon</a></div>
-            </DialogContentText>
-          </DialogContent>
         </Dialog>
       </div>
     )
