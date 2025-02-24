c50deab660ec17e30e2656d218394e4ed935fcf4
Alexandre Magno <alexanmtz@gmail.com>
2025-02-24 15:21:39 +0100

    adding alias to config prod

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
