63363ccfee045795714a03d130d983713c1749d7
Alexandre Magno <alexanmtz@gmail.com>
2025-04-07 23:25:24 +0200

    fixing wrong login with provider connected accounts

--- config/passport.js
+++ config/passport.js
@@ -407,7 +407,7 @@ passport.use(
       try {
         const user = await userExist(userAttributes)
         if (!user) return done(null, false)
-        if (user.provider) return done(null, false)
+        if (user.login_strategy === 'github') return done(null, false)
         if (user.verifyPassword(password, user.password)) {
           const token = jwt.sign(
             { email: user.email },
