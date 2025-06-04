5ff95fffdf691766e71cffaa6624e1fcadd2a03a
Alexandre Magno <alexanmtz@gmail.com>
2025-06-04 13:22:27 +0200

    test: split actions test into specific files (#1212)

---
+++ frontend/tests/actions/contactActions.test.js
@@ -0,0 +1,54 @@
+/**
+ * @jest-environment jsdom
+ */
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import moxios from 'moxios'
+import * as contactActions from '../../src/actions/contactActions'
+import Auth from '../../src/modules/auth'
+
+Auth.getToken = () => true
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+describe('recruitment landing page actions', () => {
+  describe('message recruiters', () => {
+    it('should dispatch an action to send message to recruitment team', () => {
+      expect(contactActions.messageRecruitersRequested()).toEqual({
+        type: 'MESSAGE_RECRUITERS_REQUESTED',
+        completed: false
+      })
+      expect(contactActions.messageRecruitersSuccess()).toEqual({
+        type: 'MESSAGE_RECRUITERS_SUCCESS',
+        completed: true
+      })
+      expect(contactActions.messageRecruitersError({ code: 401 })).toEqual({
+        type: 'MESSAGE_RECRUITERS_ERROR',
+        completed: true,
+        error: { code: 401 }
+      })
+    })
+    it('should dispatch an action to send message to recruiters async', () => {
+      moxios.install()
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: {}
+        })
+      })
+      const expectedActions = [
+        { completed: false, type: 'MESSAGE_RECRUITERS_REQUESTED' },
+        { open: true, text: 'actions.message.recruiters.success', type: 'ADD_NOTIFICATION' },
+        { completed: true, type: 'MESSAGE_RECRUITERS_SUCCESS' }
+      ]
+      const store = mockStore({ intl: { messages: {} } })
+      return store.dispatch(contactActions.messageRecruiters('name', 'title', 'email', 'phone', 'company', 'country', 'message')).then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions)
+        moxios.uninstall()
+      })
+    })
+  })
+})
--- frontend/tests/actions/taskActions.test.js
+++ frontend/tests/actions/taskActions.test.js
@@ -55,4 +55,52 @@ xdescribe('task actions', () => {
       })
     })
   })
+
+  describe('message author', () => {
+    it('should dispatch an action to send message to author', () => {
+      expect(taskActions.messageAuthorRequested()).toEqual({
+        type: 'MESSAGE_AUTHOR_REQUESTED',
+        completed: false
+      })
+      expect(taskActions.messageAuthorSuccess()).toEqual({
+        type: 'MESSAGE_AUTHOR_SUCCESS',
+        completed: true
+      })
+      expect(taskActions.messageAuthorError({ code: 401 })).toEqual({
+        type: 'MESSAGE_AUTHOR_ERROR',
+        completed: true,
+        error: { code: 401 }
+      })
+    })
+    it('should dispatch an action to send message to author async', () => {
+      moxios.install()
+      moxios.stubRequest('http://localhost:3000/authenticated', {
+        status: 200,
+        response: {
+          authenticated: true,
+          user: {
+            id: 1
+          }
+        }
+      })
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: {}
+        })
+      })
+      const expectedActions = [
+        { completed: false, type: 'MESSAGE_AUTHOR_REQUESTED' },
+        { open: true, text: 'actions.task.message.author.success', type: 'ADD_NOTIFICATION' },
+        { completed: true, type: 'MESSAGE_AUTHOR_SUCCESS' }
+      ]
+      const store = mockStore({ intl: { messages: {} }, task: { completed: true, id: 1 }, loggedIn: { logged: true, user: { id: 1 } } })
+      return store.dispatch(taskActions.messageAuthor(1, 1, 'message')).then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions)
+        moxios.uninstall()
+      })
+    })
+  })
 })
