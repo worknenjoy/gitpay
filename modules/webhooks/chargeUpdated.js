5302552918571ba6cfaa8139a7ee72977fee5269
Alexandre Magno <alexanmtz@gmail.com>
2025-07-10 22:06:03 +0200

    refactor: move webhook cases to modules

---
+++ modules/webhooks/chargeUpdated.js
@@ -0,0 +1,55 @@
+const models = require('../../models')
+const i18n = require('i18n')
+const moment = require('moment')
+const SendMail = require('../mail/mail')
+const WalletMail = require('../mail/wallet')
+const stripe = require('../shared/stripe/stripe')()
+const { FAILED_REASON, CURRENCIES, formatStripeAmount } = require('./constants')
+
+module.exports = async function chargeUpdated(event, paid, status, req, res) {
+  if(event?.data?.object?.source?.id) {
+    return models.Order.update(
+      {
+        paid: paid,
+        status: status
+      },
+      {
+        where: {
+          source_id: event.data.object.source.id,
+          source: event.data.object.id
+        },
+        returning: true
+      }
+    )
+      .then(order => {
+        if (order[0]) {
+          return models.User.findOne({
+            where: {
+              id: order[1][0].dataValues.userId
+            }
+          })
+            .then(user => {
+              if (user) {
+                if (paid && status === 'succeeded') {
+                  const language = user.language || 'en'
+                  i18n.setLocale(language)
+                  SendMail.success(
+                    user.dataValues,
+                    i18n.__('mail.webhook.payment.update.subject'),
+                    i18n.__('mail.webhook.payment.update.message', { amount: event.data.object.amount / 100 })
+                  )
+                }
+              }
+              return res.status(200).json(event);
+            })
+            .catch(e => {
+              return res.status(400).send(e)
+            })
+        }
+      })
+      .catch(e => {
+        return res.status(400).send(e)
+      })
+  }
+  return res.status(200).json(event);
+}
---
+++ modules/webhooks/invoiceFinalized.js
@@ -0,0 +1,32 @@
+const models = require('../../models');
+const i18n = require('i18n');
+const moment = require('moment');
+const SendMail = require('../mail/mail');
+const WalletMail = require('../mail/wallet');
+const stripe = require('../shared/stripe/stripe')();
+const { FAILED_REASON, CURRENCIES, formatStripeAmount } = require('./constants');
+
+module.exports = async function invoiceFinalized(event, req, res) {
+        try {
+          const invoice = event.data.object
+          const invoiceId = invoice.id
+          const walletOrder = await models.WalletOrder.findOne({
+            where: {
+              source: invoiceId
+            },
+            include: [
+              {
+              model: models.Wallet,
+              include: [models.User]
+              }
+            ]
+            })
+          if(walletOrder?.id) {
+            WalletMail.invoiceCreated(invoice,  walletOrder, walletOrder.Wallet.User)
+            return res.status(200).json(event);
+          }
+        } catch (error) {
+          console.log('error', error)
+          return res.status(200).json(event);
+        }
+}
