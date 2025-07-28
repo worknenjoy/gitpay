ef5cfe20b9efff9c27444596c58145914e75c1ab
Alexandre Magno <alexanmtz@gmail.com>
2025-07-28 19:16:08 +0200

    fixing storytypes issue when deploy to production

--- frontend/src/components/design-library/organisms/forms/payout-forms/payout-schedule-form/payout-schedule-form.stories.tsx
+++ frontend/src/components/design-library/organisms/forms/payout-forms/payout-schedule-form/payout-schedule-form.stories.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 import PayoutScheduleForm from './payout-schedule-form';
 
 export default {
   title: 'Design Library/Organisms/Forms/PayoutForms/PayoutScheduleForm',
   component: PayoutScheduleForm
-} as ComponentMeta<typeof PayoutScheduleForm>;
+};
 
-const Template: ComponentStory<typeof PayoutScheduleForm> = (args) => (
+const Template = (args) => (
   <PayoutScheduleForm {...args} />
 );
 
