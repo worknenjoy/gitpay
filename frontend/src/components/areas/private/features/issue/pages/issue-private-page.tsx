132fddabb31cdee0ab49f0d3a434da51ec33c3d7
Alexandre Magno <alexanmtz@gmail.com>
2025-10-04 13:34:44 +0200

    private and public issue page working on render

---
+++ frontend/src/components/areas/private/features/issue/pages/issue-private-page.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import IssuePrivatePageComponent from 'design-library/organisms/layouts/page-layouts/issue-page-layout/issue-page-layout'
+import { useParams } from 'react-router-dom';
+
+const IssuePrivatePage = (props) => {
+  const { fetchTask, syncTask } = props;
+  const { id } = useParams<{ id: string }>();
+  
+  useEffect(() => {
+    if (fetchTask) {
+      fetchTask(id);
+    }
+    if (syncTask) {
+      syncTask(id);
+    }
+  }, []);
+  
+
+  return (
+    <IssuePrivatePageComponent
+      { ...props }
+    />
+  );
+}
+
+export default IssuePrivatePage;
