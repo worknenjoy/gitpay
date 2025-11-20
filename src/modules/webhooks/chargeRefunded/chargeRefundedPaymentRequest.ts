7018d63260ce5f2cb4cc4ca1b64114bd14d2d4ca
Alexandre Magno <alexanmtz@gmail.com>
2025-11-20 17:41:16 +0100

    fix email notification issue

--- src/modules/webhooks/chargeRefunded/chargeRefundedPaymentRequest.ts
+++ src/modules/webhooks/chargeRefunded/chargeRefundedPaymentRequest.ts
@@ -1,5 +1,6 @@
 
 const Models = require('../../../models');
+import { sumBalanceTransactionFeesForRange } from '../../../scripts/reports/revenue-reports/lib/stripe';
 import PaymentRequestMail from '../../mail/paymentRequest';
 import Stripe from '../../shared/stripe/stripe'
 import { handleAmount } from '../../util/handle-amount/handle-amount';
@@ -11,6 +12,13 @@ export const handleChargeRefundedPaymentRequest = async (payment_intent:any) => 
   const { metadata } = payment_intent || {};
 
   const paymentRequestPaymentId = metadata.payment_request_payment_id;
+  const userId = metadata.user_id;
+
+  const user = await models.User.findOne({
+    where: {
+      id: userId
+    }
+  });
 
   const paymentRequestPayment = await models.PaymentRequestPayment.findOne({
     where: {
@@ -52,7 +60,7 @@ export const handleChargeRefundedPaymentRequest = async (payment_intent:any) => 
     if (paymentRequest) {
       const paymentRequestBalance = await models.PaymentRequestBalance.findOrCreate({
         where: {
-          userId: paymentRequest.userId
+          userId: user.id
         }
       });
 
@@ -71,13 +79,22 @@ export const handleChargeRefundedPaymentRequest = async (payment_intent:any) => 
         closedAt: Math.floor(Date.now() / 1000)
       });
 
-      console.log(`Created PaymentRequestBalanceTransaction for Refund on PaymentRequest ID: ${paymentRequestBalanceTransactionForRefund.id}`);
+      const balanceTransactionUpdated = await models.PaymentRequestBalanceTransaction.findOne({
+        where: {
+          id: paymentRequestBalanceTransactionForRefund.id
+        },
+        include: [
+          {
+            model: models.PaymentRequestBalance
+          }
+        ]
+      });
       
       if(paymentRequestBalanceTransactionForRefund?.id) {
         PaymentRequestMail.newBalanceTransactionForPaymentRequest(
-          paymentRequest.userId,
+          user,
           paymentRequestPayment,
-          paymentRequestBalanceTransactionForRefund
+          balanceTransactionUpdated
         ).catch((mailError: any) => {
           console.error(`Failed to send email for Refund on PaymentRequest ID: ${paymentRequestBalanceTransactionForRefund.id}`, mailError);
         });
