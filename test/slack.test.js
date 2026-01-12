cba69fb7a25bf93b70dcd3a667c3c5711fce55ac
devmaster-x <shcherbakarina6@gmail.com>
2026-01-12 13:05:29 +0800

    Update slack notification test

--- test/slack.test.js
+++ test/slack.test.js
@@ -1,19 +1,8 @@
-const request = require('supertest')
 const expect = require('chai').expect
 const nock = require('nock')
-const api = require('../src/server').default
-const agent = request.agent(api)
-const models = require('../src/models')
-const { registerAndLogin, truncateModels } = require('./helpers')
 const { notifyNewIssue, notifyNewBounty } = require('../src/modules/slack')
 
 describe('Slack Notifications', () => {
-  beforeEach(async () => {
-    await truncateModels(models.Task)
-    await truncateModels(models.User)
-    await truncateModels(models.Order)
-  })
-
   afterEach(() => nock.cleanAll())
 
   describe('New Issue Notifications', () => {
@@ -22,30 +11,12 @@ describe('Slack Notifications', () => {
         .post('/services/T08RJTHD0JG/B094MSLS6HM/t6zpqBEQEk8D96YIK0gIRLU4')
         .reply(200, 'ok')
 
-      const user = await registerAndLogin(agent)
-      
-      nock('https://api.github.com')
-        .get('/repos/test/repo/issues/123')
-        .query(true)
-        .reply(200, { title: 'Test Issue', body: 'Test description', state: 'open' })
-      
-      nock('https://api.github.com')
-        .get('/repos/test/repo/languages')
-        .query(true)
-        .reply(200, { JavaScript: 100 })
+      await notifyNewIssue(
+        { id: 1, title: 'Test Issue', description: 'Test description' },
+        { username: 'testuser' }
+      )
 
-      nock('https://api.github.com')
-        .get('/repos/test/repo')
-        .query(true)
-        .reply(200, { name: 'repo', owner: { login: 'test' } })
-
-      const res = await agent
-        .post('/tasks/create')
-        .send({ url: 'https://github.com/test/repo/issues/123', provider: 'github' })
-        .set('Authorization', user.headers.authorization)
-        .expect(200)
-
-      expect(res.body.title).to.equal('Test Issue')
+      expect(slackWebhook.isDone()).to.be.true
     })
 
     it('should handle missing task data gracefully', async () => {
@@ -60,25 +31,13 @@ describe('Slack Notifications', () => {
         .post('/services/T08RJTHD0JG/B094MSLS6HM/t6zpqBEQEk8D96YIK0gIRLU4')
         .reply(200, 'ok')
 
-      const user = await registerAndLogin(agent)
-      const task = await models.Task.create({
-        url: 'https://github.com/test/repo/issues/1',
-        userId: user.body.id,
-        title: 'Test Bounty Task'
-      })
+      await notifyNewBounty(
+        { id: 1, title: 'Test Task' },
+        { amount: 100, currency: 'USD' },
+        { username: 'testuser' }
+      )
 
-      const res = await agent
-        .post('/orders')
-        .send({
-          source_id: 'test_payment',
-          currency: 'USD',
-          amount: 100,
-          taskId: task.id
-        })
-        .set('Authorization', user.headers.authorization)
-        .expect(200)
-
-      expect(res.body.amount).to.equal('100')
+      expect(slackWebhook.isDone()).to.be.true
     })
 
     it('should handle missing order data gracefully', async () => {
