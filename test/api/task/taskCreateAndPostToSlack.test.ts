7ca142fe132e5a49a9600de1adc564696bfc4c91
devmaster-x <shcherbakarina6@gmail.com>
2026-01-16 02:15:48 +0800

    update test

--- test/api/task/taskCreateAndPostToSlack.test.ts
+++ test/api/task/taskCreateAndPostToSlack.test.ts
@@ -4,9 +4,6 @@ import request from 'supertest'
 import api from '../../../src/server'
 import { registerAndLogin, truncateModels } from '../../helpers'
 import Models from '../../../src/models'
-// Use require to avoid TS type dependency on @types/sinon
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const sinon = require('sinon')
 const secrets = require('../../../src/config/secrets')
 const getSingleIssue = require('../../data/github/github.issue.get')
 const getSingleRepo = require('../../data/github/github.repository.get')
@@ -50,141 +47,89 @@ describe('Task Creation and Slack Notifications', () => {
 
   afterEach(() => {
     nock.cleanAll()
-    sinon.restore()
+    delete process.env.SLACK_WEBHOOK_URL
   })
 
   it('should not call Slack methods when task is created with not_listed set to true', async () => {
-    // Clear require cache to ensure fresh module load with stub
-    delete require.cache[require.resolve('../../../src/modules/slack')]
-    delete require.cache[require.resolve('../../../src/modules/slack/index')]
-    delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-    delete require.cache[require.resolve('../../../src/modules/tasks')]
-    delete require.cache[require.resolve('../../../src/app/controllers/task')]
-
     // Setup GitHub API mocks
     setupGitHubMocks(999)
 
-    // Stub the Slack notification method
-    const SlackModule = require('../../../src/modules/slack')
-    const slackStub = sinon.stub(SlackModule, 'notifyNewIssue').resolves(true)
+    // Mock Slack webhook (following slack.test.js pattern)
+    process.env.SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/TEST/WEBHOOK/URL'
+    const slackWebhook = nock('https://hooks.slack.com')
+      .post('/services/TEST/WEBHOOK/URL')
+      .reply(200, 'ok')
 
     const user = await registerAndLogin(agent)
     const { headers } = user || {}
 
-    try {
-      await agent
-        .post('/tasks/create')
-        .send({
-          url: 'https://github.com/worknenjoy/gitpay/issues/999',
-          provider: 'github',
-          not_listed: true
-        })
-        .set('Authorization', headers.authorization)
-        .expect('Content-Type', /json/)
-        .expect(200)
+    await agent
+      .post('/tasks/create')
+      .send({
+        url: 'https://github.com/worknenjoy/gitpay/issues/999',
+        provider: 'github',
+        not_listed: true
+      })
+      .set('Authorization', headers.authorization)
+      .expect('Content-Type', /json/)
+      .expect(200)
 
-      // Assert that notifyNewIssue was not called
-      expect(slackStub.called).to.equal(false)
-    } finally {
-      slackStub.restore()
-      // Restore cache
-      delete require.cache[require.resolve('../../../src/modules/slack')]
-      delete require.cache[require.resolve('../../../src/modules/slack/index')]
-      delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-      delete require.cache[require.resolve('../../../src/modules/tasks')]
-      delete require.cache[require.resolve('../../../src/app/controllers/task')]
-    }
+    // Assert that Slack webhook was not called
+    expect(slackWebhook.isDone()).to.equal(false)
   })
 
   it('should not call Slack methods when task is created with private set to true', async () => {
-    // Clear require cache to ensure fresh module load with stub
-    delete require.cache[require.resolve('../../../src/modules/slack')]
-    delete require.cache[require.resolve('../../../src/modules/slack/index')]
-    delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-    delete require.cache[require.resolve('../../../src/modules/tasks')]
-    delete require.cache[require.resolve('../../../src/app/controllers/task')]
-
     // Setup GitHub API mocks
     setupGitHubMocks(998)
 
-    // Stub the Slack notification method
-    const SlackModule = require('../../../src/modules/slack')
-    const slackStub = sinon.stub(SlackModule, 'notifyNewIssue').resolves(true)
+    // Mock Slack webhook (following slack.test.js pattern)
+    process.env.SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/TEST/WEBHOOK/URL'
+    const slackWebhook = nock('https://hooks.slack.com')
+      .post('/services/TEST/WEBHOOK/URL')
+      .reply(200, 'ok')
 
     const user = await registerAndLogin(agent)
     const { headers } = user || {}
 
-    try {
-      await agent
-        .post('/tasks/create')
-        .send({
-          url: 'https://github.com/worknenjoy/gitpay/issues/998',
-          provider: 'github',
-          private: true
-        })
-        .set('Authorization', headers.authorization)
-        .expect('Content-Type', /json/)
-        .expect(200)
+    await agent
+      .post('/tasks/create')
+      .send({
+        url: 'https://github.com/worknenjoy/gitpay/issues/998',
+        provider: 'github',
+        private: true
+      })
+      .set('Authorization', headers.authorization)
+      .expect('Content-Type', /json/)
+      .expect(200)
 
-      // Assert that notifyNewIssue was not called
-      expect(slackStub.called).to.equal(false)
-    } finally {
-      slackStub.restore()
-      // Restore cache
-      delete require.cache[require.resolve('../../../src/modules/slack')]
-      delete require.cache[require.resolve('../../../src/modules/slack/index')]
-      delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-      delete require.cache[require.resolve('../../../src/modules/tasks')]
-      delete require.cache[require.resolve('../../../src/app/controllers/task')]
-    }
+    // Assert that Slack webhook was not called
+    expect(slackWebhook.isDone()).to.equal(false)
   })
 
   it('should call Slack methods when task is created as public', async () => {
-    // Clear require cache to ensure fresh module load with stub
-    delete require.cache[require.resolve('../../../src/modules/slack')]
-    delete require.cache[require.resolve('../../../src/modules/slack/index')]
-    delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-    delete require.cache[require.resolve('../../../src/modules/tasks')]
-    delete require.cache[require.resolve('../../../src/app/controllers/task')]
-
     // Setup GitHub API mocks
     setupGitHubMocks(997)
 
-    // Stub the Slack notification method
-    const SlackModule = require('../../../src/modules/slack')
-    const slackStub = sinon.stub(SlackModule, 'notifyNewIssue').resolves(true)
+    // Mock Slack webhook (following slack.test.js pattern)
+    process.env.SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/TEST/WEBHOOK/URL'
+    const slackWebhook = nock('https://hooks.slack.com')
+      .post('/services/TEST/WEBHOOK/URL')
+      .reply(200, 'ok')
 
     const user = await registerAndLogin(agent)
     const { headers, body: currentUser } = user || {}
 
-    try {
-      await agent
-        .post('/tasks/create')
-        .send({
-          url: 'https://github.com/worknenjoy/gitpay/issues/997',
-          provider: 'github'
-        })
-        .set('Authorization', headers.authorization)
-        .expect('Content-Type', /json/)
-        .expect(200)
+    await agent
+      .post('/tasks/create')
+      .send({
+        url: 'https://github.com/worknenjoy/gitpay/issues/997',
+        provider: 'github'
+      })
+      .set('Authorization', headers.authorization)
+      .expect('Content-Type', /json/)
+      .expect(200)
 
-      // Assert that notifyNewIssue was called
-      expect(slackStub.calledOnce).to.equal(true)
-
-      // Verify the call arguments
-      const [taskArg, userArg] = slackStub.firstCall.args
-      expect(taskArg).to.exist
-      expect(taskArg.id).to.exist
-      expect(userArg).to.exist
-      expect(userArg.id).to.equal(currentUser.id)
-    } finally {
-      slackStub.restore()
-      // Restore cache
-      delete require.cache[require.resolve('../../../src/modules/slack')]
-      delete require.cache[require.resolve('../../../src/modules/slack/index')]
-      delete require.cache[require.resolve('../../../src/modules/tasks/taskBuilds')]
-      delete require.cache[require.resolve('../../../src/modules/tasks')]
-      delete require.cache[require.resolve('../../../src/app/controllers/task')]
-    }
+    // Assert that Slack webhook was called
+    expect(slackWebhook.isDone()).to.equal(true)
   })
 })
