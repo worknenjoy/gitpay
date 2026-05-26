647cfb90e36df04e62da8956f7f6446ddfe798c0
Alexandre Magno <alexanmtz@gmail.com>
2026-05-26 23:33:26 +0200

    fixing payment requests total amount on platform stats

--- src/queries/stats/totalPaidForPaymentRequests.ts
+++ src/queries/stats/totalPaidForPaymentRequests.ts
@@ -5,7 +5,7 @@ const models = Models as any
 
 export const totalPaidForPaymentRequests = async (): Promise<number> => {
   const result = await models.PaymentRequestPayment.findOne({
-    where: { status: 'succeeded' },
+    where: { status: ['paid', 'succeeded'] },
     attributes: [[fn('SUM', col('amount')), 'total']]
   })
   return parseInt(result?.getDataValue('total') ?? '0', 10) || 0
