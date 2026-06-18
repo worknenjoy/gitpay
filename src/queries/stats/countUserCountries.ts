0f14f5d9c517a923930854d53b0da516352dffd6
Alexandre Magno <alexanmtz@gmail.com>
2026-06-19 00:23:24 +0200

    fixing country list that was errneous refering a frontend file from the backend

--- src/queries/stats/countUserCountries.ts
+++ src/queries/stats/countUserCountries.ts
@@ -1,5 +1,7 @@
-import { countryCodesFull } from '../../../frontend/src/components/areas/private/shared/country-codes.js'
+// Mirrors the length of countryCodesFull in frontend/src/components/areas/private/shared/country-codes.js
+// Update this when countries are added or removed from that list.
+const SUPPORTED_COUNTRIES_COUNT = 193
 
 export const countUserCountries = async (): Promise<number> => {
-  return countryCodesFull.length
+  return SUPPORTED_COUNTRIES_COUNT
 }
